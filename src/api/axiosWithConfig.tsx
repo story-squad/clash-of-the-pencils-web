1e0e017930f3745cf352214549b754911aed9f9f
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-10-29 23:32:19 -0700

    Refactored axios calls slightly.

--- src/api/axiosWithConfig.tsx
+++ src/api/axiosWithConfig.tsx
@@ -1,13 +1,15 @@
 import axios, { AxiosInstance } from 'axios';
 import { getToken } from '../utils';
 
-export const axiosWithAuth: AxiosInstance = axios.create({
-  baseURL: process.env.NODE_API_URL || 'http://localhost:5000',
-  headers: {
-    Authorization: getToken(),
-  },
-});
+export const axiosWithAuth = (): AxiosInstance =>
+  axios.create({
+    baseURL: process.env.NODE_API_URL || 'http://localhost:5000',
+    headers: {
+      Authorization: getToken(),
+    },
+  });
 
-export const axiosWithoutAuth: AxiosInstance = axios.create({
-  baseURL: process.env.NODE_API_URL || 'http://localhost:5000',
-});
+export const axiosWithoutAuth = (): AxiosInstance =>
+  axios.create({
+    baseURL: process.env.NODE_API_URL || 'http://localhost:5000',
+  });
