547fb9ef9b418ec8d5b7e5b21a8ab84e7363a6b3
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-10-31 16:33:12 -0700

    Abstracted time info into a mappable array to help implement time zones

--- src/components/pages/LandingPage/LandingSplash.tsx
+++ src/components/pages/LandingPage/LandingSplash.tsx
@@ -4,6 +4,25 @@ interface SplashProps {
   isLogin: boolean;
 }
 
+const timeList = [
+  {
+    title: 'Every Morning (7 days a week!)',
+    text: 'Sign in to get the new story-writing prompt and start scribbling',
+  },
+  {
+    title: '12:00 PST',
+    text: 'The deadline to upload your single-page story',
+  },
+  {
+    title: '12:30 PST',
+    text: 'Finalists are announced and popular voting begins',
+  },
+  {
+    title: '3:00 PST)',
+    text: 'The winner of the popular vote gets crowned!',
+  },
+];
+
 const Splash = ({ isLogin }: SplashProps): React.ReactElement => {
   return (
     <div className="splash">
@@ -29,22 +48,11 @@ const SignupSplash = () => {
       </h2>
       <h3>How It Works</h3>
       <ul>
-        <li>
-          <span className="b">Every Morning (7 days a week!):</span> Sign in to
-          get the new story-writing prompt and start scribbling
-        </li>
-        <li>
-          <span className="b">12:00 PST:</span> The deadline to upload your
-          single-page story
-        </li>
-        <li>
-          <span className="b">12:30 PST:</span> Finalists are announced and
-          popular voting begins
-        </li>
-        <li>
-          <span className="b">13:00 PST:</span> The winner of the popular vote
-          gets crowned!
-        </li>
+        {timeList.map((li, i) => (
+          <li key={i}>
+            <span className="b">{li.title}:</span> {li.text}
+          </li>
+        ))}
       </ul>
     </>
   );
