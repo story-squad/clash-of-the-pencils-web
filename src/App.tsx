8b7743da09b861214ae07d98bb9aca37166db8d3
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-11-02 15:05:34 -0800

    Added files for SubmissionPage.

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 import { PrivateRoute } from './utils';
 
 // Route Components
-import { LandingPageContainer } from './components/pages/LandingPage';
-import { Dashboard } from './components/pages/Dashboard';
+import { LandingPage } from './components/pages/LandingPage';
 import { Activation } from './components/pages/Activated';
+import { Dashboard } from './components/pages/Dashboard';
+import { SubmissionPage } from './components/pages/SubmissionPage';
 
 const App: React.FC = () => {
   return (
@@ -15,12 +16,13 @@ const App: React.FC = () => {
         <Route
           exact
           path={['/', '/login', '/register', '/signup']}
-          component={() => <LandingPageContainer />}
+          component={() => <LandingPage />}
         />
         <Route path="/activated/:token" component={Activation} />
 
         {/* Private Routes */}
         <PrivateRoute path="/dashboard" component={Dashboard} />
+        <PrivateRoute path="/submission" component={SubmissionPage} />
 
         {/* Fallback Redirect to Dashboard */}
         <Route path="/" component={() => <Redirect to="/dashboard" />} />
---
+++ src/components/pages/SubmissionPage/RenderSubmissionPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RenderSubmissionPage = (): React.ReactElement => {
+  return (
+    <div className="submission-page">
+      <h2>Submissions</h2>
+    </div>
+  );
+};
+
+export default RenderSubmissionPage;
