c9c217efbfc33a0ff9a218abb1ca7033afd71424
unknown <dethgrenade@gmail.com>
2020-11-03 13:57:19 -0800

    made corrections to typing in userState atoms.ts to pass checks

--- src/state/userState/atoms.ts
+++ src/state/userState/atoms.ts
@@ -2,13 +2,15 @@ import Recoil, { atom } from 'recoil';
 
 // Type definition for our userState state object
 export interface User {
+  // This is only here currently to pass checks on dev deployment
+  // the User object typing will be defined at a later date
   id: number;
 }
 
 // Initial userState when app is launched
-const initState: User | object = {};
+const initState: User | null = null;
 
-export const userState: Recoil.RecoilState<User | object> = atom({
+export const userState: Recoil.RecoilState<User | null> = atom<User | null>({
   key: 'userState',
   default: initState,
 });
