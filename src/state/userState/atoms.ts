f73a419567fd31e1cc3e588949bbe506dee5f5ba
unknown <dethgrenade@gmail.com>
2020-11-03 13:14:34 -0800

    refactored types and interface in atoms.ts for userState

--- src/state/userState/atoms.ts
+++ src/state/userState/atoms.ts
@@ -1,12 +1,14 @@
-import { atom } from 'recoil';
+import Recoil, { atom } from 'recoil';
 
 // Type definition for our userState state object
-export interface User {}
+export interface User {
+  id: number;
+}
 
 // Initial userState when app is launched
-const initState: User = {};
+const initState: User | object = {};
 
-export const userState: User = atom({
+export const userState: Recoil.RecoilState<User | object> = atom({
   key: 'userState',
   default: initState,
 });
