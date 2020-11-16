a3653ce267408562c9eb1bac2788230912841829
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-11-15 19:49:35 -0800

    Added recoil user state clear to prevent potential security vulnerability.

--- src/components/common/Signout/Signout.tsx
+++ src/components/common/Signout/Signout.tsx
@@ -1,13 +1,17 @@
 import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import { useSetRecoilState } from 'recoil';
+import { user } from '../../../state';
 import { token } from '../../../utils';
 import { Loader } from '../Loader';
 
 const Signout = (): React.ReactElement => {
   const { push } = useHistory();
+  const setUserId = useSetRecoilState(user.userId);
 
   useEffect(() => {
     setTimeout(() => {
+      setUserId(null);
       token.clear();
       push('/');
     }, 1000);
