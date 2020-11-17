f7bda75968b46c38a12e29b09f38bc26caccf019
unknown <dethgrenade@gmail.com>
2020-11-17 15:20:13 -0800

    refactored DragHandle functionality directly onto DraggableDragon component

--- src/components/pages/VotingPage/DragonBank/DraggableDragon.tsx
+++ src/components/pages/VotingPage/DragonBank/DraggableDragon.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
+import { Draggable, DraggableProvided } from 'react-beautiful-dnd';
 
 // Current emptyDragon is the wrong shape/color
 import emptyDragon from '../../../../assets/empty-dragon.svg';
 import dragonBoi from '../../../../assets/dragon-boi.svg';
 
-import { DragHandle } from '../DragHandle';
-
 const DraggableDragon = ({
   place,
   id,
+  index,
 }: DraggableDragonProps): React.ReactElement => {
   // Later this needs to be replaced with a recoil selector that checks if
   // this place has been chosen already, and conditionally renders a clear or
@@ -16,17 +16,25 @@ const DraggableDragon = ({
   const wasDragged = false;
 
   return (
-    <DragHandle id={id} index={0}>
-      <div className="draggable-dragon">
-        <img src={wasDragged ? emptyDragon : dragonBoi} alt={`${place}`} />
-      </div>
-    </DragHandle>
+    <Draggable draggableId={id} index={index}>
+      {(provided: DraggableProvided) => (
+        <div
+          className="draggable-dragon"
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          <img src={wasDragged ? emptyDragon : dragonBoi} alt={`${place}`} />
+        </div>
+      )}
+    </Draggable>
   );
 };
 
 interface DraggableDragonProps {
   place: number;
   id: string;
+  index: number;
 }
 
 export default DraggableDragon;
