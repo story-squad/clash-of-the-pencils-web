44bc58ce6fd7c0595f6085607f1391acb6a7183a
unknown <dethgrenade@gmail.com>
2020-11-17 11:05:24 -0800

    added Draggable functionality to DraggableDragon component

--- src/components/pages/VotingPage/DragonBank/DraggableDragon.tsx
+++ src/components/pages/VotingPage/DragonBank/DraggableDragon.tsx
@@ -4,8 +4,11 @@ import React from 'react';
 import emptyDragon from '../../../../assets/empty-dragon.svg';
 import dragonBoi from '../../../../assets/dragon-boi.svg';
 
+import { DragHandle } from '../DragHandle';
+
 const DraggableDragon = ({
   place,
+  id,
 }: DraggableDragonProps): React.ReactElement => {
   // Later this needs to be replaced with a recoil selector that checks if
   // this place has been chosen already, and conditionally renders a clear or
@@ -13,14 +16,17 @@ const DraggableDragon = ({
   const wasDragged = false;
 
   return (
-    <div className="draggable-dragon">
-      <img src={wasDragged ? emptyDragon : dragonBoi} alt={`${place}`} />
-    </div>
+    <DragHandle id={id} index={0}>
+      <div className="draggable-dragon">
+        <img src={wasDragged ? emptyDragon : dragonBoi} alt={`${place}`} />
+      </div>
+    </DragHandle>
   );
 };
 
 interface DraggableDragonProps {
   place: number;
+  id: string;
 }
 
 export default DraggableDragon;
