3822929acb19f6c7c615b7ef026b54f142f6342d
unknown <dethgrenade@gmail.com>
2020-11-17 11:06:09 -0800

    added DropZones to DragonBank component

--- src/components/pages/VotingPage/DragonBank/DragonBank.tsx
+++ src/components/pages/VotingPage/DragonBank/DragonBank.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import DraggableDragon from './DraggableDragon';
+import { DropZone } from '../DropZone';
 
 const DragonBank = (): React.ReactElement => {
   return (
     <div className="dragon-bank">
-      <DraggableDragon place={1} />
-      <DraggableDragon place={2} />
-      <DraggableDragon place={3} />
+      <DropZone id="vote-1">
+        <DraggableDragon id="award-1" place={1} />
+      </DropZone>
+      <DropZone id="vote-2">
+        <DraggableDragon id="award-2" place={2} />
+      </DropZone>
+      <DropZone id="vote-3">
+        <DraggableDragon id="award-3" place={3} />
+      </DropZone>
     </div>
   );
 };
