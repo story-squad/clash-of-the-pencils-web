d49cd8c1404c00a26950d400b182aad91f4fc759
unknown <dethgrenade@gmail.com>
2020-11-18 12:53:32 -0800

    added reset votes button to reset the DnD container state to default

--- src/components/pages/VotingPage/CastVote/RenderCastVotes.tsx
+++ src/components/pages/VotingPage/CastVote/RenderCastVotes.tsx
@@ -1,21 +1,35 @@
 import React from 'react';
 
-import { useSetRecoilState } from 'recoil';
-import { top3 } from '../../../../state';
+import { useRecoilState, useSetRecoilState } from 'recoil';
+import { top3, DnD } from '../../../../state';
 
 import { Header } from '../../../common';
 import { nav } from '../../../../config';
 import { DropBank } from '../DropBank';
 import { DragonBank } from '../DragonBank';
-import { AiOutlineArrowLeft as Left } from 'react-icons/ai';
+import {
+  AiOutlineArrowLeft as Left,
+  AiOutlineReload as Reload,
+} from 'react-icons/ai';
 
 const RenderCastVotes = (): React.ReactElement => {
   const setHasRead = useSetRecoilState(top3.hasFinishedReadingState);
+  const [DnDState, SetDnDState] = useRecoilState(DnD.DnDContainerState);
 
   const submitVotes = () => null;
 
   const backToRead = () => {
     setHasRead(false);
+  };
+
+  // Resets the vote choices to starting positions
+  const resetVotes = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    event.preventDefault();
+    // Hooks cannot be used inside event handlers so we aren't able
+    // to simply reset by using useResetRecoilState hook
+    SetDnDState(DnD.initDnDContainerState);
   };
 
   return (
@@ -40,11 +54,24 @@ const RenderCastVotes = (): React.ReactElement => {
             <Left />
             Go Back
           </button>
+          <button className="small" onClick={(e) => resetVotes(e)}>
+            <Reload />
+            Reset Votes
+          </button>
         </div>
         <DragonBank />
         <DropBank />
         <div className="button-container">
-          <button disabled={true} onClick={submitVotes}>
+          <button
+            disabled={
+              DnDState['sub-1'].isEmpty ||
+              DnDState['sub-2'].isEmpty ||
+              DnDState['sub-3'].isEmpty
+                ? true
+                : false
+            }
+            onClick={submitVotes}
+          >
             Vote
           </button>
         </div>
@@ -52,11 +79,5 @@ const RenderCastVotes = (): React.ReactElement => {
     </div>
   );
 };
-
-// props.DnDState['sub-1'].isEmpty ||
-// props.DnDState['sub-2'].isEmpty ||
-// props.DnDState['sub-3'].isEmpty
-//   ? true
-//   : false;
 
 export default RenderCastVotes;
