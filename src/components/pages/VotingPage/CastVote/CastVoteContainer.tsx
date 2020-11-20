4404378a1ab424296cfd933893402bd519e5e54d
unknown <dethgrenade@gmail.com>
2020-11-20 07:59:29 -0800

    refactored onDragEnd to handle if destination from result is undefined added type checking to DnDState from useRecoilState hook

--- src/components/pages/VotingPage/CastVote/CastVoteContainer.tsx
+++ src/components/pages/VotingPage/CastVote/CastVoteContainer.tsx
@@ -3,9 +3,12 @@ import { useRecoilState } from 'recoil';
 import { DnD } from '../../../../state/';
 import RenderCastVotes from './RenderCastVotes';
 import { DragDropContext, DragStart, DropResult } from 'react-beautiful-dnd';
+import { DnDContainerState } from '../../../../state/DnDState';
 
 const CastVoteContainer = (): React.ReactElement => {
-  const [DnDState, setDnDState] = useRecoilState(DnD.DnDContainerState);
+  const [DnDState, setDnDState] = useRecoilState<DnDContainerState>(
+    DnD.DnDContainerState,
+  );
   /**
    * Callback used by React Beautiful DnD to update state at the beginning of a drag event.
    * This is being used to render the dragon outline image when a user starts dragging a dragon
@@ -26,14 +29,6 @@ const CastVoteContainer = (): React.ReactElement => {
     const { source, destination, draggableId } = result;
     // if the draggable was dropped outside a droppable we don't need to do anything so return
     if (!destination) {
-      return;
-    }
-    // if the source id and index is the same as the destination id and index
-    // then it was moved around but then dropped back in the original spot
-    if (
-      source.droppableId === destination.droppableId &&
-      source.index === destination.index
-    ) {
       // update state to show the drop zone is once again occupied
       setDnDState({
         ...DnDState,
@@ -43,30 +38,47 @@ const CastVoteContainer = (): React.ReactElement => {
         },
       });
     }
-    // ref to destination state object
-    const finish = DnDState[destination.droppableId];
-    // if we have a destination and the destination contents are also empty we need
-    // to update state so the re-render will reflect the new placement of the elements
-    if (finish.isEmpty) {
-      // create a new source object of the updated contents and isEmpty
-      const newStart = {
-        contents: '',
-        isEmpty: true,
-      };
-      // create a new destination object of the updated contents and isEmpty
-      const newFinish = {
-        contents: draggableId,
-        isEmpty: false,
-      };
-      // update state with new state object
-      setDnDState({
-        // copy in the existing state object
-        ...DnDState,
-        // update the source contents
-        [source.droppableId]: newStart,
-        // update the destination contents
-        [destination.droppableId]: newFinish,
-      });
+    if (destination) {
+      // if the source id and index is the same as the destination id and index
+      // then it was moved around but then dropped back in the original spot
+      if (
+        source.droppableId === destination.droppableId &&
+        source.index === destination.index
+      ) {
+        // update state to show the drop zone is once again occupied
+        setDnDState({
+          ...DnDState,
+          [source.droppableId]: {
+            ...DnDState[source.droppableId],
+            isEmpty: false,
+          },
+        });
+      }
+      // ref to destination state object
+      const finish = DnDState[destination.droppableId];
+      // if we have a destination and the destination contents are also empty we need
+      // to update state so the re-render will reflect the new placement of the elements
+      if (finish.isEmpty) {
+        // create a new source object of the updated contents and isEmpty
+        const newStart = {
+          contents: '',
+          isEmpty: true,
+        };
+        // create a new destination object of the updated contents and isEmpty
+        const newFinish = {
+          contents: draggableId,
+          isEmpty: false,
+        };
+        // update state with new state object
+        setDnDState({
+          // copy in the existing state object
+          ...DnDState,
+          // update the source contents
+          [source.droppableId]: newStart,
+          // update the destination contents
+          [destination.droppableId]: newFinish,
+        });
+      }
     }
   };
   return (
