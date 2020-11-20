f240cd88f4f960794773e9c7f43aa40de816b606
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-11-19 21:08:45 -0800

    Added error message to voting page.

--- src/components/pages/VotingPage/CastVote/RenderCastVotes.tsx
+++ src/components/pages/VotingPage/CastVote/RenderCastVotes.tsx
@@ -13,6 +13,7 @@ import {
   AiOutlineArrowLeft as Left,
   AiOutlineReload as Reload,
 } from 'react-icons/ai';
+import { Voting } from '../../../../api';
 
 const RenderCastVotes = (): React.ReactElement => {
   const setHasRead = useSetRecoilState(top3.hasFinishedReadingState);
@@ -21,21 +22,23 @@ const RenderCastVotes = (): React.ReactElement => {
   const disableButton = useRecoilValue(DnD.disableVoteButton);
   // grab the user id from recoil to ensure we are logged in
   const userId = useRecoilValue(user.userId);
-  const top3list = useRecoilValue(top3.top3List);
+  const [error, setError] = useState<null | string>(null);
 
   //check if a user has voted
   const [voted, setVoted] = useState(false);
 
   const submitVotes = () => {
-    // API call goes in here
-    // updated the voted state to True
-    setVoted(true);
-    console.log(
-      voteSubmission,
-      top3list?.map((x) => x.id),
-    );
-    alert('We got your vote!');
-    return null;
+    setError(null);
+    Voting.submit(voteSubmission)
+      .then((res) => {
+        // SUBMISSION SUCCESSFUL!
+        setVoted(true);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        setError('Could not submit vote. Please try again later.');
+      });
   };
 
   const backToRead = () => {
@@ -49,6 +52,7 @@ const RenderCastVotes = (): React.ReactElement => {
     event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
   ): void => {
     event.preventDefault();
+    setError(null);
     resetDnD();
   };
 
@@ -60,7 +64,7 @@ const RenderCastVotes = (): React.ReactElement => {
           <h2>Welcome to</h2>
           <h1>Dragon Drop!</h1>
           <p className="instructions">
-            <span className="alt vote">Vote</span> by dragging the Drag-N-Drop
+            <span className="alt">Vote</span> by dragging the Drag-N-Drop
             Dragons onto your favorite story below.{' '}
             <span className="alt">Then</span> click the vote button!
           </p>
@@ -77,6 +81,7 @@ const RenderCastVotes = (): React.ReactElement => {
         </div>
         <DragonBank />
         <DropBank />
+        {error && <div className="error">{error}</div>}
         <div className="button-container">
           <button
             // disable the vote button if any of the submission containers don't have a vote
