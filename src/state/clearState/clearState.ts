34c343c551eb0734f00c68d875f7afbd49ef41b3
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-11-21 14:30:24 -0800

    Finished resets of recoil state.

--- src/state/clearState/clearState.ts
+++ src/state/clearState/clearState.ts
@@ -2,7 +2,17 @@ import { selector } from 'recoil';
 import { top3List, hasFinishedReadingState, hasReadState } from '../top3State';
 import { DnDContainerState } from '../DnDState';
 import { list } from '../pastSubsState';
-import { userId } from '../userState';
+import { userId, email, username } from '../userState';
+import { global } from '../apiErrorState';
+import { currentPrompt } from '../promptState';
+import { histogram, winner } from '../resultsState';
+import {
+  error,
+  loading,
+  preview,
+  selected,
+  success,
+} from '../submitModalState';
 
 // This selector exists to clear all Recoil state on logout and should be updated as state grows
 export const all = selector<null>({
@@ -15,5 +25,16 @@ export const all = selector<null>({
     reset(hasReadState);
     reset(list);
     reset(userId);
+    reset(email);
+    reset(username);
+    reset(global);
+    reset(currentPrompt);
+    reset(histogram);
+    reset(winner);
+    reset(error);
+    reset(loading);
+    reset(preview);
+    reset(selected);
+    reset(success);
   },
 });
