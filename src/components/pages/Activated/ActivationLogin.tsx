e52a5422901ff616738f7e8c5f305d179ec4b087
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-11-22 16:34:34 -0800

    FINALLY fixed the issue, it was simple all along I just didn't believe in the magic. RIP.

--- src/components/pages/Activated/ActivationLogin.tsx
+++ src/components/pages/Activated/ActivationLogin.tsx
@@ -7,26 +7,25 @@ import { Modal } from '../../common';
 export const Activation = (): React.ReactElement => {
   const [status, setStatus] = useState('Attempting to activate...');
   const { push } = useHistory();
-  const { search, pathname } = useLocation();
+  const { search } = useLocation();
 
   useEffect(() => {
     const activationCode = search.slice(1).split('=')[1];
-    console.log({ activationCode, search, pathname });
 
     if (activationCode) {
       Auth.activatedLogin(activationCode)
         .then((res) => {
           token.set(res.data.token);
           setStatus('You are now being redirected...');
-          setTimeout(() => push('/dashboard'), 20000);
+          setTimeout(() => push('/dashboard'), 3000);
         })
         .catch(() => {
-          setStatus('Activation failed. Redirecting to login...');
-          setTimeout(() => push('/'), 20000);
+          setStatus('Activation failed. Redirecting to homepage...');
+          setTimeout(() => push('/'), 3000);
         });
     } else {
-      setStatus('No activation code provided. Redirecting to login...');
-      setTimeout(() => push('/'), 20000);
+      setStatus('No activation code provided. Redirecting to homepage...');
+      setTimeout(() => push('/'), 3000);
     }
   }, []);
 
