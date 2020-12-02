f6b6087dd3bc098852d3cb657b850d669791f8ff
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-12-02 00:33:21 -0800

    More cleanup from testing.

--- src/state/top3State/top3Atoms.ts
+++ src/state/top3State/top3Atoms.ts
@@ -1,23 +1,9 @@
 import { atom } from 'recoil';
 import { Submissions } from '../../api';
 
-const makeTop3 = (userId: number) => {
-  const src = 'https://i.redd.it/54awv7jxe4e41.jpg';
-  return [...new Array(3)].map((x, i) => ({
-    id: i + 1,
-    image: '',
-    username: 'TestUsername',
-    userId: userId,
-    src: src,
-  }));
-};
-
-// const top3InitState = null;
-const top3InitState: Submissions.SubItem[] = makeTop3(14);
-
 export const top3List = atom<Submissions.SubItem[] | null>({
   key: 'top3List',
-  default: top3InitState,
+  default: null,
 });
 
 const initialHasReadState = [false, false, false];
