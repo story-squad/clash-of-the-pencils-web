2d3d1a0a326adb288a5da903357a7045d07ed56b
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-12-03 23:10:41 -0800

    Updated submissions tooltip.

--- src/config/tooltips/tooltips.ts
+++ src/config/tooltips/tooltips.ts
@@ -2,3 +2,8 @@ export const subInstructions =
   "First, read the prompt. Then, write a one-page story by hand. \
 When you're done, take a picture and upload it to our site. \
 Happy writing!";
+
+export const filetypes =
+  'Please make sure your story is an image file. Currenty accepted \
+  file types are JPG, PNG, and HEIC. We do not currently accept PDF \
+  uploads!';
