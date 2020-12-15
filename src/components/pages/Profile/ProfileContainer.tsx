c9a89f9a0ce4d5dc0ad9463c0b6956ff3a83b168
Ben <bigriffin94@outlook.com>
2020-12-14 16:09:51 -0800

    EoD commit

--- src/components/pages/Profile/ProfileContainer.tsx
+++ src/components/pages/Profile/ProfileContainer.tsx
@@ -1,14 +1,15 @@
 import React, { useEffect } from 'react';
 import { useRecoilState } from 'recoil';
-
-import { pastSubs } from '../../../state';
 import { Submissions } from '../../../api';
-
-import RenderProfile from './RenderProfile';
+import { pastSubs, user } from '../../../state';
 import { Loader } from '../../common';
+import RenderProfile from './RenderProfile';
 
 const Profile: React.FC = () => {
+  // pull list of users top 5 stories from recoil state && the API call getRecentSubsByChild()
   const [list, setList] = useRecoilState(pastSubs.list);
+  // pull users email from state and update the users email from the API call udpateEmail()
+  const [email, setEmail] = useRecoilState(user.email);
 
   useEffect(() => {
     Submissions.getRecentSubsByChild()
@@ -19,6 +20,16 @@ const Profile: React.FC = () => {
         console.log(err);
       });
   }, []);
+
+  // useEffect(() => {
+  //   Users.udpateEmail()
+  //     .then((userEmail) => {
+  //       setEmail(userEmail);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }, []);
 
   return list ? <RenderProfile picList={list} /> : <Loader />;
 };
