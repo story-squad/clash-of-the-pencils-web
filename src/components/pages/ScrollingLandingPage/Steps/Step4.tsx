4c4e1ab7c1170541f7c4b64b829400168c84dee7
Drew Ross <mail.drew.ross@gmail.com>
2020-12-15 14:21:37 -0800

    Replace header with img. Add story submission image.

--- src/components/pages/ScrollingLandingPage/Steps/Step4.tsx
+++ src/components/pages/ScrollingLandingPage/Steps/Step4.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import submitPic from '../../../../assets/img/submit-a-pic.png';
+import writingArrowSubmit from '../../../../assets/img/writing-arrow-submit.png';
 import { DragonBoi } from '../Home/DragonBoi';
 
 const Step4 = (): React.ReactElement => {
   return (
     <div className="step-4">
       <h2>Step 4</h2>
-      <h3>
-        Scribble Down a Story
-        <br />
-        (1-page <u>by hand</u>)
-      </h3>
+      <img
+        className="img-center"
+        src={submitPic}
+        alt="3. Submit a pic of your story by the deadline"
+      />
+      <img
+        className="img-center"
+        src={writingArrowSubmit}
+        alt="Arrow pointing from hand-written story to a button with text: Submit Your Story"
+      />
       <DragonBoi />
     </div>
   );
