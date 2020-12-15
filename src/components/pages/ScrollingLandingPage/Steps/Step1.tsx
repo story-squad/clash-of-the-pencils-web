d9373ff9b00e705010b1f79146faad5d15c3e64a
Drew Ross <mail.drew.ross@gmail.com>
2020-12-15 11:01:07 -0800

    Add page header, PromptBox, DragonBoi.

--- src/components/pages/ScrollingLandingPage/Steps/Step1.tsx
+++ src/components/pages/ScrollingLandingPage/Steps/Step1.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { PromptBox } from '../../../common/PromptBox';
+import { DragonBoi } from '../Home/DragonBoi';
 
 const Step1 = (): React.ReactElement => {
   return (
     <div className="step-1">
       <h2>Step 1</h2>
+      <h3>Scope Out the Propmt</h3>
+      <PromptBox />
+      <DragonBoi />
     </div>
   );
 };
