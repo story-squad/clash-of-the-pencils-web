e1394aad902dcf8ba460b717e8cf2bde2e767721
Drew Ross <mail.drew.ross@gmail.com>
2020-12-15 14:14:19 -0800

    Fix className from file copy.

--- src/components/pages/ScrollingLandingPage/Steps/Step4.tsx
+++ src/components/pages/ScrollingLandingPage/Steps/Step4.tsx
@@ -3,7 +3,7 @@ import { DragonBoi } from '../Home/DragonBoi';
 
 const Step4 = (): React.ReactElement => {
   return (
-    <div className="step-3">
+    <div className="step-4">
       <h2>Step 4</h2>
       <h3>
         Scribble Down a Story
