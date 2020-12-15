76fe50d916920f5aea1ebeae23fe50c73d887352
Drew Ross <mail.drew.ross@gmail.com>
2020-12-15 13:30:10 -0800

    Replace prompt and header with Figma images.

--- src/components/pages/ScrollingLandingPage/Steps/Step1.tsx
+++ src/components/pages/ScrollingLandingPage/Steps/Step1.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import { PromptBox } from '../../../common/PromptBox';
+import dummyPrompt from '../../../../assets/img/dummy-prompt.png';
+import scopeOut from '../../../../assets/img/scope-out-prompt.png';
 import { DragonBoi } from '../Home/DragonBoi';
 
 const Step1 = (): React.ReactElement => {
   return (
     <div className="step-1">
       <h2>Step 1</h2>
-      <h3>Scope Out the Propmt</h3>
-      <PromptBox />
+      <img
+        className="img-center"
+        src={scopeOut}
+        alt="1. Scope out the prompt."
+      />
+      <img
+        className="img-center"
+        src={dummyPrompt}
+        alt="Prompt: you're a super hero, and there is a town that needs saving."
+      />
       <DragonBoi />
     </div>
   );
