46912e74909be89d6b94e12b3dbd18edb74a09d3
Drew Ross <mail.drew.ross@gmail.com>
2020-12-15 14:12:49 -0800

    Replace header with img. Add handwriting img.

--- src/components/pages/ScrollingLandingPage/Steps/Step3.tsx
+++ src/components/pages/ScrollingLandingPage/Steps/Step3.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import scribbleDown from '../../../../assets/img/scribble-down.png';
+import writingStory from '../../../../assets/img/writing-story.png';
 import { DragonBoi } from '../Home/DragonBoi';
 
 const Step3 = (): React.ReactElement => {
   return (
     <div className="step-3">
       <h2>Step 3</h2>
-      <h3>
-        Scribble Down a Story
-        <br />
-        (1-page <u>by hand</u>)
-      </h3>
+      <img
+        className="img-center"
+        src={scribbleDown}
+        alt="2. Scribble down a story. 1 page by hand."
+      />
+      <img
+        className="img-center"
+        src={writingStory}
+        alt="Hand with pencil writing a story."
+      />
       <DragonBoi />
     </div>
   );
