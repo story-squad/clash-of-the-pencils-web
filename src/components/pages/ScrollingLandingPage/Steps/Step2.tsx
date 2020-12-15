9bb9694d00b4c3189c2c490dd60b10b0e8e04fde
Drew Ross <mail.drew.ross@gmail.com>
2020-12-15 11:19:40 -0800

    Add header and DragonBoi.

--- src/components/pages/ScrollingLandingPage/Steps/Step2.tsx
+++ src/components/pages/ScrollingLandingPage/Steps/Step2.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { DragonBoi } from '../Home/DragonBoi';
 
 const Step2 = (): React.ReactElement => {
   return (
     <div className="step-2">
       <h2>Step 2</h2>
+      <h3>
+        Go Get Your Lucky Pencil
+        <br />
+        and Some Paper!
+      </h3>
+      <DragonBoi />
     </div>
   );
 };
--- src/components/pages/ScrollingLandingPage/Steps/Step3.tsx
+++ src/components/pages/ScrollingLandingPage/Steps/Step3.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { DragonBoi } from '../Home/DragonBoi';
 
 const Step3 = (): React.ReactElement => {
   return (
     <div className="step-3">
       <h2>Step 3</h2>
+      <h3>
+        Scribble Down a Story
+        <br />
+        (1-page <u>by hand</u>)
+      </h3>
+      <DragonBoi />
     </div>
   );
 };
