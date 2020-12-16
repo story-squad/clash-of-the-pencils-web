a45c258a72a5dcb465b22af09c3606be55755021
Drew Ross <mail.drew.ross@gmail.com>
2020-12-16 11:34:06 -0800

    Add button icons and aria-label.

--- src/components/pages/LandingPage/Steps/NavArrowButton.tsx
+++ src/components/pages/LandingPage/Steps/NavArrowButton.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
 import { NavDirection } from './StepsContainer';
 
 const NavArrowButton = (props: NavArrowButtonProps): React.ReactElement => {
   const className = `nav-arrow-button ${props.navDirection}`;
+  const ariaLabel = `Navigate ${props.navDirection}`;
 
   return (
     <button
       className={className}
+      aria-label={ariaLabel}
       onClick={() => props.buttonNav(props.navDirection)}
     >
-      {props.navDirection === 'left' ? 'Left' : 'Right'}
+      {props.navDirection === 'left' ? <FaAngleLeft /> : <FaAngleRight />}
     </button>
   );
 };
