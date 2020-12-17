602b831a5ef861e6863e12e268703cf2ea54d95e
Drew Ross <mail.drew.ross@gmail.com>
2020-12-16 16:14:30 -0800

    Add buttonNavUpDown function.

--- src/components/pages/LandingPage/ScrollingLandingPageContainer.tsx
+++ src/components/pages/LandingPage/ScrollingLandingPageContainer.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import ReactScroller from 'react-page-scroller';
+import { NavDirection } from './NavArrowButton';
 import { Home } from './Home';
 import { Steps } from './Steps';
 import { VotingInfo } from './VotingInfo';
@@ -8,6 +9,13 @@ const ScrollingLandingPageContainer = (): React.ReactElement => {
   const [currentPage, setCurrentPage] = useState<number | null>(null);
   const changePage = (newNum: number) => setCurrentPage(newNum);
   const beforePageChange = (newNum: number) => console.log(newNum);
+
+  const buttonNavUpDown = (navDirection: NavDirection) => {
+    if (navDirection === 'up') {
+      changePage((currentPage ? currentPage : 0) - 1);
+    } else if (navDirection === 'down')
+      changePage((currentPage ? currentPage : 0) - 1);
+  };
 
   return (
     <ReactScroller
