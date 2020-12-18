b54f07e81fba2e93779aef0ed4bbd1c1399fd735
Drew Ross <mail.drew.ross@gmail.com>
2020-12-17 16:08:46 -0800

    Add dragon.

--- src/components/pages/LandingPage/VotingInfo/VotingInfo.tsx
+++ src/components/pages/LandingPage/VotingInfo/VotingInfo.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import voteFinalists from '../../../../assets/img/vote-on-finalists.png';
+import blazeWillRead from '../../../../assets/img/blaze-will-read.png';
+import dragonSingle from '../../../../assets/img/dragon-single-page.png';
 import dragonDropDemo from '../../../../assets/img/dragon-drop-demo.png';
 import NavArrowButton, { NavDirection } from '../NavArrowButton';
 
 const VotingInfo = (props: VotingInfoProps): React.ReactElement => {
   return (
     <div className="voting-info">
-      <img
-        className="img-center"
-        src={voteFinalists}
-        alt="4. Vote on the three finalists to determine a winner and see your squad score."
-      />
       <NavArrowButton buttonNav={props.buttonNav} navDirection="up" />
-      <img
-        className="img-center"
-        src={dragonDropDemo}
-        alt="Dragon Drop. Vote by dragging the dra-n-drop dragons onto your favorite story below, then click the button."
-      />
-      <div className="button-container">
-        <div className="link-button">
-          <Link to="/game">Get Started</Link>
+      <div className="flex-wrapper">
+        <img
+          className="img-center"
+          src={blazeWillRead}
+          alt="4. Vote on the three finalists to determine a winner and see your squad score."
+        />
+        <img
+          className="img-center"
+          src={dragonDropDemo}
+          alt="Dragon Drop. Vote by dragging the drag-n-drop dragons onto your favorite story below, then click the button."
+        />
+        <div className="button-container">
+          <div className="link-button">
+            <Link to="/game">Get Started</Link>
+          </div>
+        </div>
+        <div className="dragon-boi right">
+          <img
+            src={dragonSingle}
+            className="dragon"
+            alt="dragon saying 'A single handwritten page? Sounds easy enough!'"
+          />
         </div>
       </div>
     </div>
