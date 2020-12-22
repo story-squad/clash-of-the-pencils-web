65f87d1b2161a85d1afd2ea993d4335b9a2d88d9
Drew Ross <mail.drew.ross@gmail.com>
2020-12-22 10:03:02 -0800

    Refactor Home page for better vertical responsiveness on mobile.

--- src/components/pages/LandingPage/Home/Home.tsx
+++ src/components/pages/LandingPage/Home/Home.tsx
@@ -8,19 +8,19 @@ import NavArrowButton, { NavDirection } from '../NavArrowButton';
 const Home = (props: HomeProps): React.ReactElement => {
   return (
     <div className="home-page-wrapper">
-      <Header />
-      <div className="home-page">
-        <img src={landingText} alt="Unleash your creativity!" />
-        <div className="dragon-boi">
+      <div className="responsive-container">
+        <Header />
+        <div className="home-page">
+          <img src={landingText} alt="Unleash your creativity!" />
           <img
             src={hiImBlaze}
             className="dragon"
             alt="Dragon saying 'Hi, I'm Blaze, and I can't wait to read your stories!'"
           />
-        </div>
-        <div className="how-it-works">
-          <img src={howItWorks} alt="How it works" />
-          <NavArrowButton buttonNav={props.buttonNav} navDirection="down" />
+          <div className="how-it-works">
+            <img src={howItWorks} alt="How it works" />
+            <NavArrowButton buttonNav={props.buttonNav} navDirection="down" />
+          </div>
         </div>
       </div>
     </div>
