53cf6bcfec4b7a4163386ebc09d01951123e9707
jessemarek <dethgrenade@gmail.com>
2020-12-30 15:30:27 -0800

    refactored some components to React.lazy() Added Suspense wrapper for lazy load fallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import {
   AuthModal,
@@ -7,31 +7,49 @@ import {
   TermsOfService,
 } from './components/common';
 import { Activation } from './components/pages/Activated';
-import { GamePage } from './components/pages/GamePage';
-import { VotingPage } from './components/pages/GamePage/VotingPage';
 import { Scroller } from './components/pages/LandingPage';
-import { ResetPasswordPage } from './components/pages/ResetPassword';
-import { ResultsPage } from './components/pages/ResultsPage';
+const VotingPage = React.lazy(() =>
+  import('./components/pages/GamePage/VotingPage').then(({ VotingPage }) => ({
+    default: VotingPage,
+  })),
+);
+const ResultsPage = React.lazy(() =>
+  import('./components/pages/ResultsPage').then(({ ResultsPage }) => ({
+    default: ResultsPage,
+  })),
+);
+const GamePage = React.lazy(() =>
+  import('./components/pages/GamePage').then(({ GamePage }) => ({
+    default: GamePage,
+  })),
+);
+const ResetPasswordPage = React.lazy(() =>
+  import('./components/pages/ResetPassword').then(({ ResetPasswordPage }) => ({
+    default: ResetPasswordPage,
+  })),
+);
 
 const App = (): React.ReactElement => {
   return (
     <div className="App">
-      <AuthModal />
-      <ReadTokenData />
-      <Switch>
-        {/* Public Routes */}
-        <Route exact path="/" component={Scroller} />
-        <Route path="/activated" component={Activation} />
-        <Route path={['/logout', '/signout']} component={Signout} />
-        <Route path="/vote" component={VotingPage} />
-        <Route path="/tos" component={TermsOfService} />
-        <Route path="/game" component={GamePage} />
-        <Route path="/results" component={ResultsPage} />
-        <Route path="/reset" component={ResetPasswordPage} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <AuthModal />
+        <ReadTokenData />
+        <Switch>
+          {/* Public Routes */}
+          <Route exact path="/" component={Scroller} />
+          <Route path="/activated" component={Activation} />
+          <Route path={['/logout', '/signout']} component={Signout} />
+          <Route path="/vote" component={VotingPage} />
+          <Route path="/tos" component={TermsOfService} />
+          <Route path="/game" component={GamePage} />
+          <Route path="/results" component={ResultsPage} />
+          <Route path="/reset" component={ResetPasswordPage} />
 
-        {/* Fallback Redirect to Dashboard */}
-        <Route path="/" component={() => <Redirect to="/game" />} />
-      </Switch>
+          {/* Fallback Redirect to Dashboard */}
+          <Route path="/" component={() => <Redirect to="/game" />} />
+        </Switch>
+      </Suspense>
     </div>
   );
 };
