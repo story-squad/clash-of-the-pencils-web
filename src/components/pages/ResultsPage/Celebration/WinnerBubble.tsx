a1f0f545016a90f302eee38ed95428519951be31
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-12-29 17:06:56 -0800

    Fixed today/yesterday issue witha BANDAID

--- src/components/pages/ResultsPage/Celebration/WinnerBubble.tsx
+++ src/components/pages/ResultsPage/Celebration/WinnerBubble.tsx
@@ -1,14 +1,9 @@
-import moment from 'moment';
 import React from 'react';
-import { time } from '../../../../utils';
 
 const WinnerBubble = (props: WinnerBubbleProps): React.ReactElement => {
-  const now = moment();
   return (
     <div className="winner-bubble">
-      <h3>
-        {now < time.schedule.announce.start ? "Yesterday's" : "Today's"} Winner:
-      </h3>
+      <h3>Current Winner:</h3>
       <h2>{props.winnerName}</h2>
       <div className="flames">&#128293; &#128293; &#128293;</div>
     </div>
