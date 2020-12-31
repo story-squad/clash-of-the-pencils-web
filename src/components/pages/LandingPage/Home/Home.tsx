06311bdcb96c61dcdb85120a70fd2d2dcdabc980
jessemarek <dethgrenade@gmail.com>
2020-12-31 15:05:41 -0800

    refactored <img>s on Home page to use <picture>s with WebPs and fallbacks to PNGs

--- src/components/pages/LandingPage/Home/Home.tsx
+++ src/components/pages/LandingPage/Home/Home.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import hiImBlaze from '../../../../assets/img/hi-im-blaze.png';
-import howItWorks from '../../../../assets/img/how-it-works.png';
-import landingText from '../../../../assets/img/landing-text.png';
-import { Header } from '../../../common';
 import { FaAngleDown } from 'react-icons/fa';
+// PNG images
+import blazePNG from '../../../../assets/img/PNGs/hi-im-blaze.png';
+import howItWorksPNG from '../../../../assets/img/PNGs/how-it-works.png';
+import landingTextPNG from '../../../../assets/img/PNGs/landing-text.png';
+// WebP images
+import blazeWEBP from '../../../../assets/img/WebPs/hi-im-blaze.webp';
+import howItWorksWEBP from '../../../../assets/img/WebPs/how-it-works.webp';
+import landingTextWEBP from '../../../../assets/img/WebPs/landing-text.webp';
+import { Header } from '../../../common';
 
 const Home = (props: HomeProps): React.ReactElement => {
   return (
@@ -18,14 +23,23 @@ const Home = (props: HomeProps): React.ReactElement => {
     >
       <Header />
       <div className="home-page">
-        <img src={landingText} alt="Unleash your creativity!" />
-        <img
-          src={hiImBlaze}
-          className="dragon"
-          alt="Dragon saying 'Hi, I'm Blaze, and I can't wait to read your stories!'"
-        />
+        <picture>
+          <source srcSet={landingTextWEBP} />
+          <img src={landingTextPNG} alt="Unleash your creativity!" />
+        </picture>
+        <picture>
+          <source srcSet={blazeWEBP} type="image/webp" />
+          <img
+            src={blazePNG}
+            className="dragon"
+            alt="Dragon saying 'Hi, I'm Blaze, and I can't wait to read your stories!'"
+          />
+        </picture>
         <div className="how-it-works">
-          <img src={howItWorks} alt="How it works" />
+          <picture>
+            <source srcSet={howItWorksWEBP} />
+            <img src={howItWorksPNG} alt="How it works" />
+          </picture>
           <FaAngleDown />
         </div>
       </div>
