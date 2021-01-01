2ea66ce54195779b53402b44657d846e289832ba
jessemarek <dethgrenade@gmail.com>
2020-12-31 17:31:37 -0800

    refactored step 4 to use new Image component

--- src/components/pages/LandingPage/Steps/Step4.tsx
+++ src/components/pages/LandingPage/Steps/Step4.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
-import dragonFire from '../../../../assets/img/PNGs/dragon-fire.png';
-import submitPic from '../../../../assets/img/PNGs/submit-a-pic.png';
-import writingArrowSubmit from '../../../../assets/img/PNGs/writing-arrow-submit.png';
+// PNG Images
+import dragonFirePNG from '../../../../assets/img/PNGs/dragon-fire.png';
+import submitPicPNG from '../../../../assets/img/PNGs/submit-a-pic.png';
+import writingArrowSubmitPNG from '../../../../assets/img/PNGs/writing-arrow-submit.png';
+//WebP Images
+import dragonFireWEBP from '../../../../assets/img/WebPs/dragon-fire.webp';
+import submitPicWEBP from '../../../../assets/img/WebPs/submit-a-pic.webp';
+import writingArrowSubmitWEBP from '../../../../assets/img/WebPs/writing-arrow-submit.webp';
+//Components
+import { Image } from '../../../common';
 import NavArrowButton from '../NavArrowButton';
 import { StepProps } from './RenderSteps';
 
@@ -18,20 +25,27 @@ const Step4 = (props: StepProps): React.ReactElement => {
     >
       <div className="grid-wrapper">
         <div className="container top">
-          <img
-            src={submitPic}
+          <Image
+            webp={submitPicWEBP}
+            src={submitPicPNG}
             alt="Submit a pic of your story by the deadline"
           />
         </div>
         <div className="container middle">
           <NavArrowButton navDirection={'left'} buttonNav={props.buttonNavX} />
-          <img
-            src={writingArrowSubmit}
+          <Image
+            webp={writingArrowSubmitWEBP}
+            src={writingArrowSubmitPNG}
             alt="Arrow pointing from hand-written story to a button with text: Submit Your Story"
           />
         </div>
         <div className="container bottom">
-          <img src={dragonFire} className="dragon" alt="dragon saying 'Go!'" />
+          <Image
+            webp={dragonFireWEBP}
+            src={dragonFirePNG}
+            classes="dragon"
+            alt="dragon saying 'Go!'"
+          />
           {props.circles()}
         </div>
       </div>
