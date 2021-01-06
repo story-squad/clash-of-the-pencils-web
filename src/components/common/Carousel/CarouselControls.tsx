ecc5df8c19e0e43335c56fdf37ef00ce49eb2baf
Brandon Ramirez <bran.ramirez.don@gmail.com>
2021-01-05 19:06:22 -0800

    Added arrow buttons to the carousel and styled them correctly.

---
+++ src/components/common/Carousel/CarouselControls.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { CarouselControlsProps } from './CarouselTypes';
+
+const CarouselControls = (props: CarouselControlsProps): React.ReactElement => {
+  return (
+    <div className="carousel-controls">
+      <p>CONTROLS</p>
+    </div>
+  );
+};
+
+export default CarouselControls;
