d5ce0258707a14405dccf808ccaa54edca8257b4
Brandon Ramirez <bran.ramirez.don@gmail.com>
2021-01-05 19:06:22 -0800

    Added arrow buttons to the carousel and styled them correctly.

--- src/components/common/Carousel/CarouselContainer.tsx
+++ src/components/common/Carousel/CarouselContainer.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
 
 const CarouselContainer = ({
   children,
@@ -20,19 +21,27 @@ const CarouselContainer = ({
 
   return (
     <div className="carousel">
-      <div className="carousel-content">
-        {numItems === 0 ? (
-          <div className="carousel-card show">{children}</div>
-        ) : (
-          // We can coerce it to an array since we know it has length!
-          (children as Array<React.ReactElement>).map<React.ReactElement>(
-            (elem, i) => (
-              <div key={i} className={`carousel-card ${getClassName(i)}`}>
-                {elem}
-              </div>
-            ),
-          )
-        )}
+      <div className="carousel-main">
+        <div className="left">
+          <MdKeyboardArrowLeft />
+        </div>
+        <div className="carousel-content">
+          {numItems === 0 ? (
+            <div className="carousel-card show">{children}</div>
+          ) : (
+            // We can coerce it to an array since we know it has length!
+            (children as Array<React.ReactElement>).map<React.ReactElement>(
+              (elem, i) => (
+                <div key={i} className={`carousel-card ${getClassName(i)}`}>
+                  {elem}
+                </div>
+              ),
+            )
+          )}
+        </div>
+        <div className="right">
+          <MdKeyboardArrowRight />
+        </div>
       </div>
       <div className="arrows">
         <button onClick={prev}>Prev</button>
---
+++ src/components/common/Carousel/CarouselTypes.ts
@@ -0,0 +1,4 @@
+export interface CarouselControlsProps {
+  current: number;
+  numItems: number;
+}
