5debfea68eec15c743f15b5531bf87f3bd681a7f
Brandon Ramirez <bran.ramirez.don@gmail.com>
2021-01-07 12:54:01 -0800

    Updated login api module and LoginForm for consistency and readability.

--- src/api/Auth/login.ts
+++ src/api/Auth/login.ts
@@ -1,11 +1,17 @@
 import { AxiosResponse } from 'axios';
 import { axiosWithoutAuth } from '../axiosWithConfig';
 
-export const login = async (credentials: LoginBody): Promise<AxiosResponse> => {
+export const login = async (
+  credentials: LoginBody,
+): Promise<AxiosResponse<LoginResponse>> => {
   return axiosWithoutAuth().post('/email/login', credentials);
 };
 
 export interface LoginBody {
   email: string;
   password: string;
+}
+
+export interface LoginResponse {
+  token: string;
 }
