e96b29e6df2d4ee57227d4ea897a8187223e22a9
Brandon Ramirez <bran.ramirez.don@gmail.com>
2021-01-07 12:49:10 -0800

    Removed unused function from login module.

--- src/api/Auth/login.ts
+++ src/api/Auth/login.ts
@@ -1,12 +1,8 @@
-import { axiosWithoutAuth } from '../axiosWithConfig';
 import { AxiosResponse } from 'axios';
+import { axiosWithoutAuth } from '../axiosWithConfig';
 
 export const login = async (credentials: LoginBody): Promise<AxiosResponse> => {
   return axiosWithoutAuth().post('/email/login', credentials);
-};
-
-export const activatedLogin = (token: string): Promise<AxiosResponse> => {
-  return axiosWithoutAuth().post('/email/activatedLogin', { token });
 };
 
 export interface LoginBody {
