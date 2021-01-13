318cef3b6828ad110df2a25b34e7381618af3f81
Brandon Ramirez <bran.ramirez.don@gmail.com>
2021-01-12 19:03:09 -0800

    Added components for write, submit, vote

--- src/components/pages/LandingPage/LandingCarousel/LandingCarousel.tsx
+++ src/components/pages/LandingPage/LandingCarousel/LandingCarousel.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import firstPlaceDragon from '../../../../assets/img/first-place-dragon.png';
+import writeImage from '../../../../assets/img/handwriting.gif';
+import writingArrow from '../../../../assets/img/writing-arrow-submit.png';
 import { Carousel } from '../../../common';
+import PlayWithBlaze from './PlayWithBlaze';
 import SocialMediaGuest from './SocialMediaGuest';
+import TextWithImageCard from './TextWithImageCard';
 
 const LandingCarousel = (): React.ReactElement => {
   return (
-    <Carousel secondsToChange={6}>
-      {/* <SocialMediaGuest /> */}
+    <Carousel secondsToChange={5}>
+      <PlayWithBlaze />
       <SocialMediaGuest />
-      {/* <PlayWithBlaze /> */}
+      <TextWithImageCard text="Write" image={writeImage} />
+      <TextWithImageCard text="Submit" image={writingArrow} />
+      <TextWithImageCard text="Vote" image={firstPlaceDragon} />
     </Carousel>
   );
 };
