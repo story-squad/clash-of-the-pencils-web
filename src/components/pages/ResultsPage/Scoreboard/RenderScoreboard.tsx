0bce4857bf84cf4046b63c52a42eaa5dad448dcf
Jorge Manzur <46169161+manzur1990@users.noreply.github.com>
2021-01-12 21:33:09 -0600

    adds an info hover icon next to the weekly leaderboard header in the celebration station

--- src/components/pages/ResultsPage/Scoreboard/RenderScoreboard.tsx
+++ src/components/pages/ResultsPage/Scoreboard/RenderScoreboard.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Submissions } from '../../../../api';
-import { Table } from '../../../common';
+import { tooltips } from '../../../../config';
+import { InfoHoverTip, Table } from '../../../common';
 
 const RenderScoreboard = ({
   scoreboard,
 }: RenderScoreboardProps): React.ReactElement => {
   return (
-    <div className="scoreboard">
-      <h3>Leaderboard</h3>
-      <Table headings={Submissions.ScoreboardHeadings} rows={scoreboard} />
-    </div>
+    <>
+      <div className="scoreboard">
+        <InfoHoverTip tip={tooltips.scoreboardInstructions} position="left" />
+        <h3>Weekly Leaderboard</h3>
+        <Table headings={Submissions.ScoreboardHeadings} rows={scoreboard} />
+      </div>
+    </>
   );
 };
 
--- src/config/tooltips/tooltips.ts
+++ src/config/tooltips/tooltips.ts
@@ -7,3 +7,6 @@ export const filetypes =
   'Please make sure your story is an image file. Currenty accepted \
   file types are JPG, PNG, and HEIC. We do not currently accept PDF \
   uploads!';
+
+export const scoreboardInstructions =
+  'Blaze the Dragon gives every story you write a "Squad Score," usually between 20 and 50 points based on how much he liked it. This leaderboard adds up the TOTAL "Squad Score" for the top 10 players over the last week.  You can improve your chances of appearing here if you write and submit a story every day!';
