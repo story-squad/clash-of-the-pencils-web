859b491f5ceb7d13105ee4a7a511ddfff2d1a5c6
Brandon <bran.ramirez.don@gmail.com>
2021-08-22 16:02:13 -0700

    Table styles

--- src/components/molecules/Table/Table.stories.tsx
+++ src/components/molecules/Table/Table.stories.tsx
@@ -6,11 +6,23 @@ const Template: Story<ITableProps> = (props) => <Table {...props} />;
 
 export const Default = Template.bind({});
 Default.args = {
-  headings: ['Col 1', 'Col 2'],
+  headings: ['Col 1', 'Col 2', 'Cutoff Test'],
   rows: [
-    ['1', '2'],
-    ['2', '3'],
-    ['3', '4'],
+    [
+      '1',
+      '2',
+      'A RATHER LONG STRING OF TEXT THAT WILL HOPEFULLY BE CUT OFF AND REPLACED WITH ELLIPSES!',
+    ],
+    [
+      '2',
+      '3',
+      'A RATHER LONG STRING OF TEXT THAT WILL HOPEFULLY BE CUT OFF AND REPLACED WITH ELLIPSES!',
+    ],
+    [
+      '3',
+      '4',
+      'A RATHER LONG STRING OF TEXT THAT WILL HOPEFULLY BE CUT OFF AND REPLACED WITH ELLIPSES!',
+    ],
   ],
 };
 
