dcc882ececb487e5d8c524ad201991dcdf7a1f46
Brandon <bran.ramirez.don@gmail.com>
2021-08-29 17:24:13 -0700

    Lint cleanup

--- src/components/atoms/Dial/DialContainer.tsx
+++ src/components/atoms/Dial/DialContainer.tsx
@@ -2,6 +2,6 @@ import React, { useState } from 'react';
 import Dial from './Dial';
 
 export default function DialContainer(): React.ReactElement {
-  const [angle, setAngle] = useState(0);
+  const [angle] = useState(0);
   return <Dial angle={angle} />;
 }
