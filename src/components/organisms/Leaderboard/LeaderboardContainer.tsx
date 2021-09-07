08e49c4ab393dc2c5f34e822e1b71694c4ad0f9e
Brandon <bran.ramirez.don@gmail.com>
2021-09-07 09:45:19 -0700

    Test connection fix

--- src/components/organisms/Leaderboard/LeaderboardContainer.tsx
+++ src/components/organisms/Leaderboard/LeaderboardContainer.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { useRecoilState } from 'recoil';
+import { leaderboardData } from '../../../data';
 import { leaderboard } from '../../../state';
 import Leaderboard from './Leaderboard';
 
 export default function LeaderboardContainer(): React.ReactElement {
-  const daily = useRecoilValue(leaderboard.daily);
-  const weekly = useRecoilValue(leaderboard.weekly);
+  // const daily = useRecoilValue(leaderboard.daily);
+  // const weekly = useRecoilValue(leaderboard.weekly);
+  const { daily, weekly } = leaderboardData;
   const [dailyIsOpen, setDailyIsOpen] = useRecoilState(leaderboard.dailyIsOpen);
 
   const toggleLeaderboard = () => setDailyIsOpen((is) => !is);
@@ -13,8 +15,8 @@ export default function LeaderboardContainer(): React.ReactElement {
   return (
     <Leaderboard
       dailyIsOpen={dailyIsOpen}
-      daily={[]}
-      weekly={[]}
+      daily={daily}
+      weekly={weekly}
       toggleLeaderboard={toggleLeaderboard}
     />
   );
