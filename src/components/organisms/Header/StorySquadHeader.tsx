705b1b83cc07dbeabac2924edb385962d46043a1
Brandon <bran.ramirez.don@gmail.com>
2021-09-11 12:42:08 -0700

    Added alternate header for auth forms

--- src/components/organisms/Header/StorySquadHeader.tsx
+++ src/components/organisms/Header/StorySquadHeader.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { pencilMonsterHead } from '../../../assets';
+import './styles/storySquadHeader.scss';
 
 export default function StorySquadHeader(): React.ReactElement {
-  return <header></header>;
+  return (
+    <header className="story-squad-header-wrapper">
+      <div className="story-squad-header">
+        <img src={pencilMonsterHead} alt="Our pencil monster's head" />
+        <h1>Story Squad</h1>
+      </div>
+    </header>
+  );
 }
