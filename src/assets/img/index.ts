9ba23d6dc6f9f6f508b8ed1a0da01490e66500fa
Brandon <bran.ramirez.don@gmail.com>
2021-09-12 13:10:10 -0700

    Added clever logo icon

--- src/assets/img/index.ts
+++ src/assets/img/index.ts
@@ -1,5 +1,6 @@
 export * as Badges from './badges';
 export { ReactComponent as ClashLogo } from './clash-logo.svg';
+export { default as cleverLogo } from './clever-logo.png';
 export { ReactComponent as ClockFaceLines } from './clock-face-lines.svg';
 export { default as emptyCardBackground } from './empty-card-background.svg';
 export { default as handwritingExampleRotated } from './handwriting-example-rotated.jpg';
