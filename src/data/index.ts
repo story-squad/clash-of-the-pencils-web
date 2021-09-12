982f6577489a7aec9d8b7b964c755fd7112ebb64
Brandon <bran.ramirez.don@gmail.com>
2021-09-12 12:44:32 -0700

    Added user data to storybook data folder

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,3 +2,4 @@ export { default as leaderboardData } from './leaderboardData';
 export { default as LoremIpsum } from './LoremIpsum';
 export { default as promptData } from './promptData';
 export { default as submissionData } from './submissionData';
+export { default as userData } from './userData';
