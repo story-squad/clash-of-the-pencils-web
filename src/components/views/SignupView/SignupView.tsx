26e94d2e40d58d939a8673c3bc3f6859ae7728f9
Brandon <bran.ramirez.don@gmail.com>
2021-09-15 15:25:36 -0700

    export

--- src/components/views/SignupView/SignupView.tsx
+++ src/components/views/SignupView/SignupView.tsx
@@ -1,13 +1,38 @@
-import React from 'react';
-import { Users } from '../../../api';
+import React, { useCallback } from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
+import { useSetRecoilState } from 'recoil';
+import { Auth, Users } from '../../../api';
+import { auth } from '../../../state';
+import { SignupForm } from '../../forms';
+import { DashboardTemplate } from '../../templates';
 
 export interface SignupViewProps {
   onSubmit?: (data: Users.INewUser) => void;
-  openLoging?: () => void;
+  openLogin?: () => void;
 }
 
-export default function SignupView({}: SignupViewProps): React.ReactElement {
+export default function SignupView({
+  onSubmit,
+  openLogin,
+}: SignupViewProps): React.ReactElement {
+  const methods = useForm();
+
+  const login = useSetRecoilState(auth.login);
+
+  const submitHandler = useCallback(
+    onSubmit ??
+      (async (data: Users.INewUser) => {
+        const res = await Auth.signup(data);
+        login(res);
+      }),
+    [login, onSubmit],
+  );
+
   return (
-    
-  )
+    <FormProvider {...methods}>
+      <DashboardTemplate useStorySquadHeader className="signup-view">
+        <SignupForm onSubmit={submitHandler} />
+      </DashboardTemplate>
+    </FormProvider>
+  );
 }
