632126aceb3b3a1477ff91114fa3538ce42db3f0
Brandon <bran.ramirez.don@gmail.com>
2021-09-21 15:51:09 -0700

    Added TOS link to footer

--- src/components/organisms/Footer/FooterNav.tsx
+++ src/components/organisms/Footer/FooterNav.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function FooterNav(): React.ReactElement {
   return (
     <nav>
       <ul>
-        <li>
+        {/* <li>
           <a>Contact</a>
-        </li>
+        </li> */}
         <li>
-          <a>Terms</a>
+          <Link to="/termsofservice" target="_blank">
+            Terms
+          </Link>
         </li>
         <li className="hide-on-tablet">&#169;2021 Story Squad HQ</li>
       </ul>
