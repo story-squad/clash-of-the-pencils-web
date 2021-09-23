ce457b1c6e4c6b7e47d229d62a9294f09c7feec6
Brandon <bran.ramirez.don@gmail.com>
2021-09-23 09:57:16 -0700

    Loader now accepts custom classes

--- src/components/molecules/Loader/Loader.tsx
+++ src/components/molecules/Loader/Loader.tsx
@@ -1,3 +1,4 @@
+import { classnames } from '@story-squad/react-utils';
 import React from 'react';
 import Dots from './Dots';
 import { ILoaderProps } from './loaderTypes';
@@ -7,10 +8,11 @@ const Loader = ({
   message = 'Loading',
   hideDots = false,
   center = true,
+  className,
   children,
 }: React.PropsWithChildren<ILoaderProps>): React.ReactElement => {
   return (
-    <div className={`loader-wrapper${center ? ' center' : ''}`}>
+    <div className={classnames('loader-wrapper', { center }, className)}>
       <div className="loader-container">
         <div className="message">
           {message}
