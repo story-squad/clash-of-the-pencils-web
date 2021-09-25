63f6d0e7bc43c92a0a21c5f5f988c9c3c426e358
Brandon <bran.ramirez.don@gmail.com>
2021-09-24 17:51:59 -0700

    Time offset cannot be changed in production due to errors!

--- src/config/appTimeOffset.ts
+++ src/config/appTimeOffset.ts
@@ -9,8 +9,10 @@ const APP_OFFSET_HOURS = 0;
 const APP_OFFSET_MINUTES = 0;
 const APP_OFFSET_SECONDS = 0;
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export const APP_TIME_OFFSET: DurationInput = {
-  hours: APP_OFFSET_HOURS,
-  minutes: APP_OFFSET_MINUTES,
-  seconds: APP_OFFSET_SECONDS,
+  hours: isProduction ? 0 : APP_OFFSET_HOURS,
+  minutes: isProduction ? 0 : APP_OFFSET_MINUTES,
+  seconds: isProduction ? 0 : APP_OFFSET_SECONDS,
 };
