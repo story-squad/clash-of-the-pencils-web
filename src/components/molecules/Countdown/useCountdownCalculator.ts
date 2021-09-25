6193146e02b8a0293f859ac799ff3e836343006a
Brandon <bran.ramirez.don@gmail.com>
2021-09-24 23:40:39 -0700

    Add offset

--- src/components/molecules/Countdown/useCountdownCalculator.ts
+++ src/components/molecules/Countdown/useCountdownCalculator.ts
@@ -1,4 +1,5 @@
 import { DateTime } from 'luxon';
+import { APP_TIME_OFFSET } from '../../../config';
 import { ICountdownProps } from './Countdown';
 
 export type UseCountdownCalculatorReturn = [
@@ -9,7 +10,7 @@ export type UseCountdownCalculatorReturn = [
 export default function useCountdownCalculator({
   endTime,
   startTime,
-  now = DateTime.utc(),
+  now = DateTime.utc().plus(APP_TIME_OFFSET),
 }: ICountdownProps): UseCountdownCalculatorReturn {
   const diffTotalInMS = Math.abs(startTime.diff(endTime).milliseconds);
   const diffNowFromStartInMS = Math.abs(startTime.diff(now).milliseconds);
