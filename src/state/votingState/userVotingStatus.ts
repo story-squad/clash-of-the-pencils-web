19930a3dffcd1e1c3f7c60c37aa56e26d378f89b
Brandon <bran.ramirez.don@gmail.com>
2021-09-24 17:16:39 -0700

    Reading won't mark stories read outside vote time

--- src/state/votingState/userVotingStatus.ts
+++ src/state/votingState/userVotingStatus.ts
@@ -1,4 +1,6 @@
 import { atomFamily, selector } from 'recoil';
+import { time } from '../../utils';
+import { now } from '../appState';
 
 export const hasReadSubInPosition = atomFamily<boolean, number>({
   key: 'hasReadSubInPositionAtomFamily',
@@ -8,6 +10,13 @@ export const hasReadSubInPosition = atomFamily<boolean, number>({
 export const hasReadAll = selector<boolean>({
   key: 'hasReadAllSubsSelector',
   get: ({ get }) => {
+    // Stories should not be marked as read outside of the voting phase
+    const currentTime = get(now);
+    const currentPhase = time.getCurrent({ now: currentTime });
+    // So if it's not the voting phase, always return false
+    if (currentPhase !== 'vote') return false;
+
+    // Otherwise, check the status of all 3 stories
     const a = get(hasReadSubInPosition(1));
     const b = get(hasReadSubInPosition(2));
     const c = get(hasReadSubInPosition(3));
