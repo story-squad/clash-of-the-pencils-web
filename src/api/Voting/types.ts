b6f9bf9d21dc923fd50f5e55bdda2fc3fa770a30
Brandon <bran.ramirez.don@gmail.com>
2021-09-29 02:26:30 -0700

    Added api call and types

--- src/api/Voting/types.ts
+++ src/api/Voting/types.ts
@@ -5,3 +5,15 @@ export interface IVotingResponse {
   tomorrow: string;
   message: string;
 }
+
+export interface IVote extends INewVote {
+  id: number;
+  created_at: Date;
+}
+
+export interface INewVote {
+  userId?: number;
+  firstPlaceId: number;
+  secondPlaceId: number;
+  thirdPlaceId: number;
+}
