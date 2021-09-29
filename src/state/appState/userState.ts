c4457ff5fa00b4a25a49053a064c83ab9b1b89dc
Brandon <bran.ramirez.don@gmail.com>
2021-09-29 10:49:19 -0700

    Will not fail for non logged in users anymore

--- src/state/appState/userState.ts
+++ src/state/appState/userState.ts
@@ -1,11 +1,21 @@
 import { atom, selector } from 'recoil';
+import { auth } from '..';
 import { Submissions, Voting } from '../../api';
 
 export const userSubForToday = atom<Submissions.ISubItem | undefined>({
   key: 'usersSubForTodayAtom',
   default: selector({
     key: 'userSubForTodayDefaultSelector',
-    get: Submissions.getUserSubForToday,
+    get: async ({ get }) => {
+      const loggedInUser = get(auth.user);
+      // If not logged in, don't try authenticated API call! Just say no!
+      if (!loggedInUser) return undefined;
+      else {
+        // If they are logged in, we can try the API call
+        const sub = await Submissions.getUserSubForToday();
+        return sub;
+      }
+    },
   }),
 });
 
