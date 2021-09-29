78d54f572344fd49e1f4ca2283d5b1653e2dda83
Brandon <bran.ramirez.don@gmail.com>
2021-09-29 02:24:38 -0700

    Added new API call and types

--- src/state/appState/userState.ts
+++ src/state/appState/userState.ts
@@ -1,11 +1,24 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
+import { Voting } from '../../api';
+
+export const userSubForToday = atom < Submission;
 
 export const userHasSubmitted = atom<boolean>({
   key: 'userHasSubmittedAtom',
   default: false,
 });
 
-export const userHasVoted = atom<boolean>({
-  key: 'userHasVotedAtom',
-  default: false,
+// Gets the user's vote from the server, or undefined
+export const userVotes = atom<Voting.IVote | undefined>({
+  key: 'userVotesAtom',
+  default: selector({
+    key: 'userVotesDefaultSelector',
+    get: Voting.getUserVoteForToday,
+  }),
+});
+
+// Checks if the user has voted based on their vote
+export const userHasVoted = selector<boolean>({
+  key: 'userHasVotedSelector',
+  get: ({ get }) => !!get(userVotes),
 });
