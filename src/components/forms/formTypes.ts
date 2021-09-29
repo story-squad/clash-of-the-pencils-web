2d005f13b1ba1e462b385fe03a2349e8b095cc60
Brandon <bran.ramirez.don@gmail.com>
2021-09-29 11:43:09 -0700

    More customizable typing

--- src/components/forms/formTypes.ts
+++ src/components/forms/formTypes.ts
@@ -2,7 +2,10 @@ export type FormOnSubmit<FormData = unknown, Response = unknown> = (
   data: FormData,
 ) => Response | Promise<Response>;
 export type FormOnError = (err: unknown) => void;
-export interface FormProps<FormData extends unknown = unknown> {
-  onSubmit: FormOnSubmit<FormData>;
+export interface FormProps<
+  FormData extends unknown = unknown,
+  Response = unknown,
+> {
+  onSubmit: FormOnSubmit<FormData, Response>;
   onError?: FormOnError;
 }
