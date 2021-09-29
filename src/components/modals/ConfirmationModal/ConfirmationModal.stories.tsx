7190a1cbbddf2c600a14c45cae403f4285ff05d3
Brandon <bran.ramirez.don@gmail.com>
2021-09-29 15:20:11 -0700

    Slightly more interactive story

--- src/components/modals/ConfirmationModal/ConfirmationModal.stories.tsx
+++ src/components/modals/ConfirmationModal/ConfirmationModal.stories.tsx
@@ -3,18 +3,25 @@ import React, { useState } from 'react';
 import { Button } from '../../atoms';
 import ConfirmationModal, { ConfirmationModalProps } from './ConfirmationModal';
 
-const Template: Story<ConfirmationModalProps> = ({
+const Template: Story<
+  ConfirmationModalProps & {
+    buttonText?: string;
+    modalStartsOpen?: boolean;
+  }
+> = ({
   message = 'Do you want to continue?',
   cancelText,
   confirmText,
   onConfirm = () => alert('Confirmed!'),
+  buttonText = 'Open Modal',
+  modalStartsOpen = true,
 }) => {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(modalStartsOpen);
   const openModal = () => setIsOpen(true);
 
   return (
     <div>
-      <Button onClick={openModal}>Open Modal</Button>
+      <Button onClick={openModal}>{buttonText}</Button>
       <ConfirmationModal
         isOpen={isOpen}
         setIsOpen={setIsOpen}
@@ -35,6 +42,8 @@ Logout.args = {
   confirmText: 'Yes',
   message: 'Are you sure you want to log out?',
   onConfirm: () => alert('Logged out!'),
+  buttonText: 'Log Out',
+  modalStartsOpen: false,
 };
 
 export default {
