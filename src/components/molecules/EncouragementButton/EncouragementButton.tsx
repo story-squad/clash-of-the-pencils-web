f7532b5620a8cea3f6737c9335ca7c5d190a0124
Brandon Ramirez <bran.ramirez.don@gmail.com>
2021-09-30 11:45:58 -0700

    Encouragement Button (#139)
    * Added use-sound hook lib
    
    * Added audio files for encouragement
    
    * Added encouragement button component
    
    * Story for encouragement button testing
    
    * Prompt box uses new encouragement button
    
    * Stream time bugfix

---
+++ src/components/molecules/EncouragementButton/EncouragementButton.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import useSound from 'use-sound';
+import { encouragement } from '../../../assets';
+import { Button } from '../../atoms';
+
+export default function EncouragementButton(): React.ReactElement {
+  // Store our currently selected audio file in state
+  const [sound, setSound] = useState(getRandomSound());
+
+  const [play, { stop }] = useSound(sound);
+
+  // This stops the currently playing sound whenever sound changes
+  useEffect(() => {
+    return stop;
+  }, [sound]);
+
+  // Custom play handler to make sure we have no overlap of sounds
+  const playHandler = () => {
+    // Set a new random sound -> this stops currently playing sound!
+    setSound(getRandomSound());
+    // Play the sound
+    play();
+  };
+
+  return (
+    <Button type="secondary" onClick={playHandler}>
+      Encouragement Button
+    </Button>
+  );
+}
+
+function getRandomSound() {
+  // Get an array of all sounds from encouragement
+  const keys = Object.keys(encouragement);
+  // Generate a random key from the encouragement module
+  const randomKey = keys[
+    (keys.length * Math.random()) << 0
+  ] as keyof typeof encouragement;
+
+  // Return the src string at the random key
+  return encouragement[randomKey];
+}
