54153043faaf81a9cae8526292d89f6b005b4955
Brandon <bran.ramirez.don@gmail.com>
2021-09-29 17:37:28 -0700

    Added and styled dashboard-wide error boundary

--- src/components/templates/Dashboard/DashboardTemplate.tsx
+++ src/components/templates/Dashboard/DashboardTemplate.tsx
@@ -1,6 +1,7 @@
-import { classnames } from '@story-squad/react-utils';
+import { classnames, ErrorBoundary } from '@story-squad/react-utils';
 import React from 'react';
 import { Footer, Header, HeaderSwitcherProps } from '../../organisms';
+import DashboardErrorFallback from './DashboardErrorFallback';
 import './styles/index.scss';
 
 export default function DashboardTemplate({
@@ -16,7 +17,11 @@ export default function DashboardTemplate({
     <div className={classnames('dashboard-template', className)}>
       <Header useStorySquadHeader={useStorySquadHeader} />
       <div id="dashboard-content">
-        <div className="dashboard-content-container">{children}</div>
+        <div className="dashboard-content-container">
+          <ErrorBoundary fallback={DashboardErrorFallback}>
+            {children}
+          </ErrorBoundary>
+        </div>
       </div>
       <Footer />
     </div>
