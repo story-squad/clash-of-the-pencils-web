ce63043ac45c8c393e25d67575e77b7722073820
Brandon <bran.ramirez.don@gmail.com>
2021-09-29 22:27:20 -0700

    Modal improvement -> can now be non-closable

--- src/components/organisms/Modal/Modal.tsx
+++ src/components/organisms/Modal/Modal.tsx
@@ -8,6 +8,7 @@ export interface ModalProps {
   isOpen: boolean;
   setIsOpen: React.Dispatch<boolean>;
   component: React.ComponentType<ModalComponentProps>;
+  closable?: boolean;
 }
 
 export interface ModalComponentProps {
@@ -18,19 +19,31 @@ export default function Modal({
   component: Component,
   isOpen,
   setIsOpen,
+  closable = true,
 }: ModalProps): React.ReactElement {
-  const closeModal = useCallback(() => setIsOpen(false), [setIsOpen]);
-  const [ref] = useClickOutside({ onClick: closeModal, isActive: isOpen });
+  const closeModal = useCallback(() => {
+    if (closable) setIsOpen(false);
+  }, [setIsOpen, closable]);
+
+  const [ref] = useClickOutside({
+    onClick: closeModal,
+    isActive: isOpen && closable,
+  });
+
   useKey({ action: closeModal, key: 'Escape' });
 
   return (
-    <div className={classnames('modal-wrapper', !isOpen && 'hidden')}>
+    <div
+      className={classnames('modal-wrapper', !isOpen && 'hidden', { closable })}
+    >
       <div className={classnames('modal')} onClick={stopPropagation} ref={ref}>
-        <img
-          src={orangeCloseButton}
-          className="modal-close-button"
-          onClick={closeModal}
-        />
+        {closable && (
+          <img
+            src={orangeCloseButton}
+            className="modal-close-button"
+            onClick={closeModal}
+          />
+        )}
         <div className="modal-content">
           <Component closeModal={closeModal} />
         </div>
