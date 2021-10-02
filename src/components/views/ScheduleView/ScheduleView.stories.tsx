00a99cdf2205952b16f35cd28e6532f2aa9ee929
Brandon <bran.ramirez.don@gmail.com>
2021-10-02 13:02:08 -0700

    Added missing story for schedule view

---
+++ src/components/views/ScheduleView/ScheduleView.stories.tsx
@@ -0,0 +1,21 @@
+import { Meta, Story } from '@storybook/react';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+import ScheduleView from './ScheduleView';
+
+const Template: Story = () => <ScheduleView openDashboard={() => undefined} />;
+
+export const Default = Template.bind({});
+
+export default {
+  title: 'Views/Schedule',
+  component: ScheduleView,
+  decorators: [
+    (story) => (
+      <RecoilRoot>
+        <BrowserRouter>{story()}</BrowserRouter>
+      </RecoilRoot>
+    ),
+  ],
+} as Meta;
