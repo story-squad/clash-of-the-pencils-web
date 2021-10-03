a38ae99ad5efeb0ae50b4bd6684ce0212407775c
Brandon <bran.ramirez.don@gmail.com>
2021-10-02 17:20:25 -0700

    Added hook for easy confirmation modal

---
+++ src/hooks/useConfirmationModal/useConfirmationModal.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import { ConfirmationModal } from '../../components/modals/';
+import { UseConfirmationModalParams } from './types';
+
+export default function useConfirmationModal(
+  modalProps: UseConfirmationModalParams,
+): [modalComponent: React.ReactElement, openModal: () => void] {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const openConfirmationModal = () => setModalIsOpen(true);
+  return [
+    <ConfirmationModal
+      key={0}
+      isOpen={modalIsOpen}
+      setIsOpen={setModalIsOpen}
+      {...modalProps}
+    />,
+    openConfirmationModal,
+  ];
+}
