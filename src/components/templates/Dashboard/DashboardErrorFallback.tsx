a3206ed29fc2f4595152f9014e3fe2ffaccd1488
Brandon <bran.ramirez.don@gmail.com>
2021-10-06 10:10:14 -0700

    Added error dragon to dashboard error

--- src/components/templates/Dashboard/DashboardErrorFallback.tsx
+++ src/components/templates/Dashboard/DashboardErrorFallback.tsx
@@ -1,6 +1,7 @@
 import { IErrorFallbackProps } from '@story-squad/react-utils/dist/utils/ErrorBoundary/types';
 import React from 'react';
-import { Button } from '../../atoms';
+import { errorDragon } from '../../../assets';
+import { Button, Picture } from '../../atoms';
 import './styles/dashboardErrorFallback.scss';
 
 export default function DashboardErrorFallback({
@@ -12,6 +13,11 @@ export default function DashboardErrorFallback({
       <h2>Uh Oh!</h2>
       <p>Something went wrong:</p>
       <p>{error.message}</p>
+      <Picture
+        source={errorDragon}
+        description="Blaze the dragon in a box"
+        disablePreview
+      />
       <Button onClick={reload}>Reload Dashboard</Button>
     </div>
   );
