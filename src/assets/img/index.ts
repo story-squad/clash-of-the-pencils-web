df4a8757a379d091cfec37f4939acf2edd1f4dbd
Brandon <bran.ramirez.don@gmail.com>
2021-10-06 10:05:03 -0700

    Added new error dragon asset

--- src/assets/img/index.ts
+++ src/assets/img/index.ts
@@ -3,6 +3,7 @@ export { ReactComponent as ClashLogo } from './clash-logo.svg';
 export { default as cleverLogo } from './clever-logo.png';
 export { ReactComponent as ClockFaceLines } from './clock-face-lines.svg';
 export { default as emptyCardBackground } from './empty-card-background.svg';
+export { ReactComponent as ErrorDragon } from './error-dragon.svg';
 export { default as greenBulletPoint } from './green-bullet-point.svg';
 export { default as handwritingExampleRotated } from './handwriting-example-rotated.jpg';
 export { default as handwritingExample } from './handwriting-example.jpg';
