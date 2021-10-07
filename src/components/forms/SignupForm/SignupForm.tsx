c2d41b705b78d4d1c7dcbe498749c7aca3cc5859
Jason Long <jlong5795@gmail.com>
2021-10-07 18:05:28 -0500

    Adds signup form validation. Missing parent email

--- src/components/forms/SignupForm/SignupForm.tsx
+++ src/components/forms/SignupForm/SignupForm.tsx
@@ -1,11 +1,12 @@
 import { ErrorMessage } from '@hookform/error-message';
 import { useAsync } from '@story-squad/react-utils';
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { useFormContext } from 'react-hook-form';
 import { Auth, Users } from '../../../api';
 import { Button, CleverButton, LoadIcon } from '../../atoms';
 import { FormProps } from '../formTypes';
 import { authFormInputs } from '../inputs';
+import { dataConstraints } from '../../../config';
 import './styles/index.scss';
 
 export type SignupFormProps = FormProps<Users.INewUser>;
@@ -15,9 +16,14 @@ export default function SignupForm({
   onError,
 }: SignupFormProps): React.ReactElement {
   // Standard form handlers
-  const { handleSubmit, setError, clearErrors } = useFormContext();
+  const { handleSubmit, setError, clearErrors, watch } = useFormContext();
   // Clearing form error
   const clearFormError = () => clearErrors('form');
+
+  // ref to password
+  const password = useRef({});
+  password.current = watch('password', '');
+
   // Custom error handler
   const errorHandler = useCallback(
     onError ?? // If a custom error handler was provided, use it instead of our function
@@ -42,8 +48,16 @@ export default function SignupForm({
     errorHandler,
   });
 
+  // calculate age
+  function calculate_age(dob: string) {
+    const diff_ms = Date.now() - new Date(dob).getTime();
+    const age_dt = new Date(diff_ms);
+
+    return Math.abs(age_dt.getUTCFullYear() - 1970);
+  }
+
   return (
-    <form className="signup-form" onSubmit={exec}>
+    <form className="signup-form" onSubmit={exec} noValidate>
       <CleverButton htmlType="button" />
       <p className="alt-font">or</p>
       <p className="main-font">Sign In Using Email Address</p>
@@ -54,15 +68,53 @@ export default function SignupForm({
       {/* First page */}
       {authFormInputs.firstname()}
       {authFormInputs.lastname()}
-      {authFormInputs.codename()}
-      {authFormInputs.birthday()}
+      {authFormInputs.codename({
+        rules: {
+          validate: {
+            checkCharacters: (value) => {
+              return (
+                dataConstraints.codenamePattern.test(value) ||
+                'Only letters and numbers are allowed!'
+              );
+            },
+            checkLength: (value) => {
+              return value.length < 15 || 'Cannot be more than 15 characters!';
+            },
+          },
+        },
+      })}
+      {authFormInputs.birthday({
+        rules: {
+          validate: {
+            checkAge: (value) => {
+              const age = calculate_age(value);
+              return age > 12 || "Parent's email is required!";
+            },
+          },
+        },
+      })}
       {/* <Button onClick={nextPage} htmlType="button">
             Next
           </Button> */}
       {/* Second page */}
-      {authFormInputs.email()}
+      {authFormInputs.email({
+        rules: {
+          pattern: {
+            value: dataConstraints.emailPattern,
+            message: 'Please enter a valid email address!',
+          },
+        },
+      })}
       {authFormInputs.password()}
-      {authFormInputs.confirmPassword()}
+      {authFormInputs.confirmPassword({
+        rules: {
+          validate: {
+            checkPassword: (value) => {
+              return password.current === value || 'Passwords do not match.';
+            },
+          },
+        },
+      })}
       {/* <Button onClick={prevPage} htmlType="button" type="secondary">
             Back
           </Button> */}
