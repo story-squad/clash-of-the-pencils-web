520d607a69a6ea02881e261ebd509e1b9cb76fc5
Brandon <bran.ramirez.don@gmail.com>
2021-10-14 18:14:15 -0700

    Made lots of progress on stream view

--- src/components/organisms/Stream/Stream.tsx
+++ src/components/organisms/Stream/Stream.tsx
@@ -1,28 +1,65 @@
-import React from 'react';
-import { Button } from '../../atoms';
+import React, { useState } from 'react';
+import ReactPlayer from 'react-player';
 import './styles/index.scss';
 
-export interface IStreamProps {
-  channelId?: string;
-  apiKey?: string;
-}
+const PlayerState = {
+  UNSTARTED: -1,
+  ENDED: 0,
+  PLAYING: 1,
+  PAUSED: 2,
+  BUFFERING: 3,
+  CUED: 5,
+} as const;
 
-export default function Stream({
-  apiKey,
-  channelId,
-}: IStreamProps): React.ReactElement {
-  const revealChampion = () => undefined;
+const streamURL =
+  'https://www.youtube.com/embed/live_stream?channel=UC0oA3AzYAj0Rnfg2Yhvrnqw';
+
+export default function Stream(): React.ReactElement {
+  const [renderFallback, setRenderFallback] = useState(false);
+  const hidePlayer = () => setRenderFallback(true);
+
+  const [playing, setPlaying] = useState(false);
+  const stop = () => setPlaying(false);
+  const start = () => setPlaying(true);
+
   return (
-    <div className="stream-wrapper">
-      <div className="stream">
-        {/* <ReactLivestream
-          platform="youtube"
-          youtubeApiKey={apiKey}
-          youtubeChannelId={channelId}
-          offlineComponent={OfflineStreamComponent}
-        /> */}
-        <Button onClick={revealChampion}>Reveal Champion</Button>
-      </div>
+    <div className="stream">
+      {renderFallback ? (
+        <>
+          <h2>Latest Livestream</h2>
+          <OfflineStreamComponent />
+        </>
+      ) : (
+        <>
+          <h2>Stream is Live!</h2>
+          <ReactPlayer
+            playing={playing}
+            onReady={start}
+            url={streamURL}
+            fallback={<OfflineStreamComponent />}
+            config={{
+              youtube: {
+                onUnstarted: hidePlayer,
+                playerVars: {
+                  events: {
+                    onStateChange: (
+                      stateVal: typeof PlayerState[keyof typeof PlayerState],
+                    ) => {
+                      console.log('[YTSTCH]', { stateVal });
+                      if (stateVal === PlayerState.ENDED) hidePlayer();
+                    },
+                    onError: (e: unknown) => {
+                      stop();
+                      hidePlayer();
+                      console.log('[YTERR]', e);
+                    },
+                  },
+                },
+              },
+            }}
+          />
+        </>
+      )}
     </div>
   );
 }
@@ -30,7 +67,8 @@ export default function Stream({
 export function OfflineStreamComponent(): React.ReactElement {
   return (
     <div className="offline-stream-component">
-      <p>Stream is currently offline</p>
+      <p>Stream is now offline</p>
+      {/* TODO put the past stream vid here */}
     </div>
   );
 }
