9a565aad633b7880b1a77420a53e98a27c2f728f
Brandon <bran.ramirez.don@gmail.com>
2021-10-15 13:34:50 -0700

    Fixes and cleanup

--- src/components/organisms/Stream/Stream.tsx
+++ src/components/organisms/Stream/Stream.tsx
@@ -3,66 +3,37 @@ import ReactPlayer from 'react-player';
 import { default as OfflineStreamVideo } from './OfflineStreamVideo';
 import './styles/index.scss';
 
-const PlayerState = {
-  UNSTARTED: -1,
-  ENDED: 0,
-  PLAYING: 1,
-  PAUSED: 2,
-  BUFFERING: 3,
-  CUED: 5,
-} as const;
-
 const streamURL = process.env.REACT_APP_LIVESTREAM_URL;
 
 export default function Stream(): React.ReactElement {
-  const [renderFallback, setRenderFallback] = useState(streamURL === undefined);
-  const hidePlayer = () => setRenderFallback(true);
-
   const [playing, setPlaying] = useState(false);
-  const stop = () => setPlaying(false);
-  const start = () => setPlaying(true);
+  const [renderFallback, setRenderFallback] = useState(streamURL === undefined);
+  const hidePlayer = () => {
+    setRenderFallback(true);
+  };
+  const onReady = () => {
+    setPlaying(true);
+  };
 
   return (
-    <div className="stream">
-      {renderFallback ? (
-        <>
-          <h2>Latest Livestream</h2>
+    <div className="stream-wrapper">
+      <div className="stream">
+        <h2>
+          {playing && !renderFallback ? 'Streaming Now!' : 'Latest Livestream'}
+        </h2>
+        {renderFallback ? (
           <OfflineStreamVideo />
-        </>
-      ) : (
-        <>
-          <h2>Stream is Live!</h2>
+        ) : (
           <ReactPlayer
             url={streamURL}
             playing={playing}
-            onReady={start}
-            config={{
-              youtube: {
-                // TODO Figure out how much of this is actually working
-                onUnstarted: (...a) => {
-                  console.log('[YTUNSTRT]', a);
-                  hidePlayer();
-                },
-                playerVars: {
-                  events: {
-                    onStateChange: (
-                      stateVal: typeof PlayerState[keyof typeof PlayerState],
-                    ) => {
-                      console.log('[YTSTCH]', { stateVal });
-                      if (stateVal === PlayerState.ENDED) hidePlayer();
-                    },
-                    onError: (e: unknown) => {
-                      console.log('[YTERR]', e);
-                      stop();
-                      hidePlayer();
-                    },
-                  },
-                },
-              },
-            }}
+            onReady={onReady}
+            onError={hidePlayer}
+            stopOnUnmount
+            config={{ youtube: { onUnstarted: hidePlayer } }}
           />
-        </>
-      )}
+        )}
+      </div>
     </div>
   );
 }
