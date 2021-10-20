7512ec2c4d9ba030df01baa73b39b4aecef67dae
Brandon <bran.ramirez.don@gmail.com>
2021-10-19 20:01:58 -0700

    Update view dragon loader and style

--- src/components/views/WinnersView/WinnersViewContainer.tsx
+++ src/components/views/WinnersView/WinnersViewContainer.tsx
@@ -4,9 +4,15 @@ import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { Submissions } from '../../../api';
 import { submissions } from '../../../state';
 import { DragonLoader } from '../../molecules';
+import { DashboardTemplate } from '../../templates';
+import './styles/winnersViewLoader.scss';
 import WinnersView from './WinnersView';
 
-const WINNER_LOADER = <DragonLoader className="winners-view-loader" />;
+const WINNER_LOADER = (
+  <DashboardTemplate>
+    <DragonLoader className="winners-view-loader" />
+  </DashboardTemplate>
+);
 
 function WinnersViewContainer(): React.ReactElement {
   const winnerIds = useRecoilValue(submissions.winners.list);
