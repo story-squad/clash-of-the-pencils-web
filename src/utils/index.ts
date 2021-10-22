de45cf8f2f3bc3fd1a81b29705997d11ad7a4ca4
Brandon <bran.ramirez.don@gmail.com>
2021-10-22 11:58:29 -0700

    Reimplemented typed query parser

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,6 @@
 export * from './helpers';
 export * as cookie from './hideCookiePopup';
+export * from './queryParser';
 export * from './sleep';
 export * as time from './time';
 export * as token from './token';
