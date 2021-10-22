3ec90dc4e7ab1be479569d8a2ebca767a1554f35
Brandon <bran.ramirez.don@gmail.com>
2021-10-22 12:02:54 -0700

    Refactored header nav item syntax

--- src/components/organisms/Header/AppNavItems.tsx
+++ src/components/organisms/Header/AppNavItems.tsx
@@ -1,17 +1,22 @@
+import { classnames } from '@story-squad/react-utils';
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import HeaderContext from './headerContext';
 
-export default function AppNavItems(): React.ReactElement {
+export default function AppNavItems({
+  className,
+}: {
+  className?: string;
+} = {}): React.ReactElement {
   const { closeMenu } = useContext(HeaderContext);
   return (
     <>
-      <li>
+      <li className={classnames('app-nav', className)}>
         <Link to="/winners" onClick={closeMenu}>
           View Winners
         </Link>
       </li>
-      <li>
+      <li className={classnames('app-nav', className)}>
         <Link to="/schedule" onClick={closeMenu}>
           Daily Schedule
         </Link>
--- src/components/organisms/Header/AuthNavItems.tsx
+++ src/components/organisms/Header/AuthNavItems.tsx
@@ -1,21 +1,28 @@
+import { classnames } from '@story-squad/react-utils';
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import HeaderContext from './headerContext';
 
-export default function AuthNavItems(): React.ReactElement {
+export default function AuthNavItems({
+  className,
+}: {
+  className?: string;
+} = {}): React.ReactElement {
   const { user, closeMenu } = useContext(HeaderContext);
   return user ? (
     <>
-      <li>Welcome, {user.codename}</li>
+      <li className={classnames('app-nav', className)}>
+        Welcome, {user.codename}
+      </li>
     </>
   ) : (
     <>
-      <li>
+      <li className={classnames('app-nav', className)}>
         <Link to="/login" onClick={closeMenu}>
           Log In
         </Link>
       </li>
-      <li>
+      <li className={classnames('app-nav', className)}>
         <Link to="/signup" onClick={closeMenu}>
           Sign Up
         </Link>
