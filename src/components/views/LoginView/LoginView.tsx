2651102783c9eac923e3bd7b53a306cb7bd53b0d
Brandon <bran.ramirez.don@gmail.com>
2021-10-22 13:33:00 -0700

    Login view can now perform merge logins

--- src/components/views/LoginView/LoginView.tsx
+++ src/components/views/LoginView/LoginView.tsx
@@ -2,8 +2,9 @@ import React, { useCallback } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 import { useHistory } from 'react-router-dom';
 import { useSetRecoilState } from 'recoil';
-import { Auth } from '../../../api';
+import { Auth, Clever } from '../../../api';
 import { auth } from '../../../state';
+import { CleverButton } from '../../atoms';
 import { LoginForm } from '../../forms';
 import { DashboardTemplate } from '../../templates';
 import './styles/index.scss';
@@ -16,7 +17,10 @@ export interface LoginViewProps {
 export default function LoginView({
   onSubmit,
   openSignup,
-}: LoginViewProps): React.ReactElement {
+  isMerge = false,
+  cleverId,
+  codename,
+}: LoginViewProps & Clever.MergeRedirectState): React.ReactElement {
   // Get form methods for the provider
   const methods = useForm();
   const { push } = useHistory();
@@ -28,17 +32,40 @@ export default function LoginView({
   const submitHandler = useCallback(
     onSubmit ??
       (async (data: Auth.ILoginBody) => {
-        const res = await Auth.login(data);
+        const res = await (() => {
+          if (isMerge && cleverId) return Clever.mergeAccounts(data, cleverId);
+          else return Auth.login(data);
+        })();
         login(res);
         push('/');
       }),
-    [login, onSubmit],
+    [login, onSubmit, isMerge, cleverId],
   );
 
   return (
     <FormProvider {...methods}>
       <DashboardTemplate useStorySquadHeader className="login-view">
-        <LoginForm onSubmit={submitHandler} />
+        <div className="login-header">
+          {isMerge ? (
+            <>
+              {codename && <h2>Hey, {codename}!</h2>}
+              <p>
+                <em>It looks like you already have a Story Squad account!</em>
+              </p>
+              <p>
+                Enter your password below to automatically link your Clever and
+                Story Squad accounts.
+              </p>
+            </>
+          ) : (
+            <>
+              <CleverButton htmlType="button" />
+              <p className="alt-font">or</p>
+              <p className="main-font">Sign In Using Story Squad Account</p>
+            </>
+          )}
+        </div>
+        <LoginForm onSubmit={submitHandler} defaultValues={{ codename }} />
         <p className="form-footer">
           Need to create an account?{' '}
           <span onClick={openSignup}>Sign Up Here</span>
