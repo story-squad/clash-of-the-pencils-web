48a586c11c978e253b7f3c029aea73e7f913a14a
Brandon <bran.ramirez.don@gmail.com>
2021-10-24 11:52:57 -0700

    Finish isWeekend selector

--- src/state/appState/timeState.ts
+++ src/state/appState/timeState.ts
@@ -2,6 +2,7 @@ import { DateTime } from 'luxon';
 import { atom, selector } from 'recoil';
 import { APP_TIME_OFFSET } from '../../config';
 import { time } from '../../utils';
+import { LuxonWeekdays } from '../../utils/time';
 import { recalculate } from '../effects';
 
 function getCurrentTimeWithOffset() {
@@ -14,7 +15,7 @@ export const now = atom<DateTime>({
     key: 'appNowDefaultSelector',
     get: getCurrentTimeWithOffset,
   }),
-  effects_UNSTABLE: [recalculate(getCurrentTimeWithOffset)],
+  effects_UNSTABLE: [recalculate(getCurrentTimeWithOffset, 1)],
 });
 
 export const phase = selector<Exclude<time.eventType, 'off'>>({
@@ -25,3 +26,26 @@ export const phase = selector<Exclude<time.eventType, 'off'>>({
     return phase;
   },
 });
+
+/**
+ * This selector looks at the UTC start time of our app submissions, which aligns
+ * pretty cleanly with the start of a UTC day, except for the 30 minute streamt time.
+ * Our stream time lines
+ */
+export const isWeekend = selector<boolean>({
+  key: 'isWeekendSelector',
+  get: ({ get }) => {
+    const n = get(now);
+    return (
+      n.weekday === LuxonWeekdays.Sunday ||
+      (n.weekday === LuxonWeekdays.Saturday && !isStreamTime(n)) ||
+      (n.weekday === LuxonWeekdays.Monday &&
+        n.hour < time.schedule.stream.end.hour)
+    );
+  },
+});
+
+function isStreamTime(now: DateTime): boolean {
+  const { end, start } = time.schedule.stream;
+  return now >= start && now < end;
+}
