ba68a130e5e6d76ee85537bf37c6bb8f62793363
Brandon <bran.ramirez.don@gmail.com>
2021-10-24 14:00:00 -0700

    Countdown works differently when game is off

--- src/components/molecules/Countdown/useCountdownCalculator.ts
+++ src/components/molecules/Countdown/useCountdownCalculator.ts
@@ -1,5 +1,6 @@
 import { DateTime } from 'luxon';
 import { APP_TIME_OFFSET } from '../../../config';
+import { time } from '../../../utils';
 import { ICountdownProps } from './Countdown';
 
 export type UseCountdownCalculatorReturn = [
@@ -8,10 +9,28 @@ export type UseCountdownCalculatorReturn = [
 ];
 
 export default function useCountdownCalculator({
+  phase,
+  ...params
+}: ICountdownProps): UseCountdownCalculatorReturn {
+  if (phase && phase === 'off') {
+    const { start: endTime } = time.schedule.submit;
+    const { end: startTime } = time.schedule.stream;
+    return getDiff({
+      endTime: endTime.plus({ day: 1 }),
+      startTime,
+      now: params.now,
+    });
+  } else return getDiff(params);
+}
+
+function getDiff({
   endTime,
   startTime,
   now = DateTime.utc().plus(APP_TIME_OFFSET),
-}: ICountdownProps): UseCountdownCalculatorReturn {
+}: Pick<
+  ICountdownProps,
+  'endTime' | 'startTime' | 'now'
+>): UseCountdownCalculatorReturn {
   const diffTotalInMS = Math.abs(startTime.diff(endTime).milliseconds);
   const diffNowFromStartInMS = Math.abs(startTime.diff(now).milliseconds);
   const diffNowFromEndInMS = Math.abs(endTime.diff(now).milliseconds);
