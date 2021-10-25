1645f270cfe143f9c75d312e67b1e585e203ee8f
Brandon <bran.ramirez.don@gmail.com>
2021-10-25 16:47:47 -0700

    Form on success now optionally accepts func res

--- src/components/forms/formTypes.ts
+++ src/components/forms/formTypes.ts
@@ -2,11 +2,14 @@ export type FormOnSubmit<FormData = unknown, Response = unknown> = (
   data: FormData,
 ) => Response | Promise<Response>;
 export type FormOnError = (err: unknown) => void;
+export type FormOnSuccess<Response> = (
+  response?: Response,
+) => void | Promise<void>;
 export interface FormProps<
   FormData extends unknown = unknown,
   Response = unknown,
 > {
   onSubmit: FormOnSubmit<FormData, Response>;
-  onSuccess?: () => void;
+  onSuccess?: FormOnSuccess<Response>;
   onError?: FormOnError;
 }
