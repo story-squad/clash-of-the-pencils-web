0d09c9e1b0145a6d67f6e0b19cf13189de84f904
bus42 <gbrewton1980@gmail.com>
2022-06-15 11:30:28 -0600

    Adds logout button to Auth0Login test component

--- src/components/auth0/Auth0Login.tsx
+++ src/components/auth0/Auth0Login.tsx
@@ -3,9 +3,16 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const Auth0LoginButton = (): React.ReactElement => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, logout } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Auth0 Log In</button>;
+  return (
+    <>
+      <button onClick={() => loginWithRedirect()}>Auth0 Log In</button>
+      <button onClick={() => logout({ returnTo: window.location.origin })}>
+        Logout
+      </button>
+    </>
+  );
 };
 
 export default Auth0LoginButton;
