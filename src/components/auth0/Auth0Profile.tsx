01b511af94afa51dce512aa06d7a4369baa30de4
bus42 <gbrewton1980@gmail.com>
2022-06-17 10:51:44 -0600

    + useEffect - call Auth0 for accesstoken and login

--- src/components/auth0/Auth0Profile.tsx
+++ src/components/auth0/Auth0Profile.tsx
@@ -1,11 +1,25 @@
 import { useAuth0 } from '@auth0/auth0-react';
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
 
 const Profile = (): React.ReactElement => {
-  const { user, isAuthenticated, isLoading, logout } = useAuth0();
+  const { user, isAuthenticated, isLoading, logout, getAccessTokenSilently } =
+    useAuth0();
   const handleLogoutClick = (): void => {
     logout({ returnTo: 'http://localhost:3000/auth0/auth0login' }); // must match the allowed logout URLs in Auth0 Dashboard
   };
+  useEffect(() => {
+    const accessToken = getAccessTokenSilently();
+    axios
+      .post(
+        'http://localhost:8000/api/auth/auth0Login',
+        {
+          email: user?.email,
+        },
+        { headers: { Authorization: 'Bearer ' + accessToken } },
+      ) // need to attach JWT from Auth0 to the request as bearer token
+      .then((res) => console.log(res.data));
+  }, []);
   if (isLoading) {
     return <div>Loading ...</div>;
   }
