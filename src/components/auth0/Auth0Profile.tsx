3d75427f80899824287d46eff4d2311a38d262a8
bus42 <gbrewton1980@gmail.com>
2022-06-17 15:23:26 -0600

    Adds useEffect to test return of userMetadata

--- src/components/auth0/Auth0Profile.tsx
+++ src/components/auth0/Auth0Profile.tsx
@@ -1,25 +1,55 @@
 import { useAuth0 } from '@auth0/auth0-react';
-import axios from 'axios';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Profile = (): React.ReactElement => {
   const { user, isAuthenticated, isLoading, logout, getAccessTokenSilently } =
     useAuth0();
+  const [userMetadata, setUserMetadata] = useState(null);
   const handleLogoutClick = (): void => {
     logout({ returnTo: 'http://localhost:3000/auth0/auth0login' }); // must match the allowed logout URLs in Auth0 Dashboard
   };
+  // useEffect(() => {
+  //   const accessToken = getAccessTokenSilently();
+  //   axios
+  //     .post(
+  //       'http://localhost:8000/api/auth/auth0Login',
+  //       {
+  //         email: user?.email,
+  //       },
+  //       { headers: { Authorization: `Bearer: ${accessToken}` } },
+  //     ) // need to attach JWT from Auth0 to the request as bearer token
+  //     .then((res) => console.log(res.data));
+  // }, []);
   useEffect(() => {
-    const accessToken = getAccessTokenSilently();
-    axios
-      .post(
-        'http://localhost:8000/api/auth/auth0Login',
-        {
-          email: user?.email,
-        },
-        { headers: { Authorization: `Bearer: ${accessToken}` } },
-      ) // need to attach JWT from Auth0 to the request as bearer token
-      .then((res) => console.log(res.data));
-  }, []);
+    const getUserMetadata = async () => {
+      const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+
+      try {
+        const accessToken = await getAccessTokenSilently({
+          audience: `https://${domain}/api/v2/`,
+          scope: 'read:current_user',
+        });
+
+        const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user?.sub}`;
+
+        const metadataResponse = await fetch(userDetailsByIdUrl, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+
+        const { user_metadata } = await metadataResponse.json();
+
+        setUserMetadata(user_metadata);
+      } catch (e) {
+        let message = 'Unknown Error';
+        if (e instanceof Error) message = e.message;
+        console.log({ message });
+      }
+    };
+
+    getUserMetadata();
+  }, [getAccessTokenSilently, user?.sub]);
   if (isLoading) {
     return <div>Loading ...</div>;
   }
@@ -27,8 +57,15 @@ const Profile = (): React.ReactElement => {
     return (
       <div>
         <img src={user?.picture} alt={user?.name} />
+        <hr />
         <h2>{user?.name}</h2>
         <p>{user?.email}</p>
+        {userMetadata ? (
+          <pre>{JSON.stringify(userMetadata, null, 2)}</pre>
+        ) : (
+          'No user metadata defined'
+        )}
+        <hr />
         <button onClick={handleLogoutClick}>Logout</button>
       </div>
     );
