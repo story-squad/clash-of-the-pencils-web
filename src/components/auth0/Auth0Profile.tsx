9b04ad4d643248f8bc63c6e46da073e10fd53895
Bus42 <gbrewton1980@gmail.com>
2022-06-22 16:02:07 -0600

    updates commented code in Auth0Profile

--- src/components/auth0/Auth0Profile.tsx
+++ src/components/auth0/Auth0Profile.tsx
@@ -1,5 +1,5 @@
 import { useAuth0 } from '@auth0/auth0-react';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 const Profile = (): React.ReactElement => {
   const { user, isAuthenticated, isLoading, logout, getAccessTokenSilently } =
@@ -8,70 +8,57 @@ const Profile = (): React.ReactElement => {
   const handleLogoutClick = (): void => {
     logout({ returnTo: 'http://localhost:3000/auth0/auth0login' }); // must match the allowed logout URLs in Auth0 Dashboard
   };
-  // currently working on this function to get the user's metadata from Auth0
+
+  // currently working on this function to get the user's metadata from Auth0. It currently causes the app to lose all user info
   // useEffect(() => {
-  //   const accessToken = getAccessTokenSilently();
-  //   axios
-  //     .post(
-  //       'http://localhost:8000/api/auth/auth0Login',
-  //       {
-  //         email: user?.email,
-  //       },
-  //       { headers: { Authorization: `Bearer: ${accessToken}` } },
-  //     ) // need to attach JWT from Auth0 to the request as bearer token
-  //     .then((res) => console.log(res.data));
-  // }, []);
-  useEffect(() => {
-    const getUserMetadata = async () => {
-      const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+  //   if (!isAuthenticated) return;
+  //   const getUserMetadata = async () => {
+  //     const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 
-      try {
-        const accessToken = await getAccessTokenSilently({
-          audience: `https://${domain}/api/v2/`,
-          scope: 'read:current_user',
-        });
+  //     try {
+  //       const accessToken = await getAccessTokenSilently({
+  //         audience: `https://${domain}/api/v2/`,
+  //         scope: 'read:current_user',
+  //       });
 
-        const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user?.sub}`;
+  //       const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user?.sub}`;
 
-        const metadataResponse = await fetch(userDetailsByIdUrl, {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        });
+  //       const metadataResponse = await fetch(userDetailsByIdUrl, {
+  //         headers: {
+  //           Authorization: `Bearer ${accessToken}`,
+  //         },
+  //       });
 
-        const { user_metadata } = await metadataResponse.json();
+  //       const { user_metadata } = await metadataResponse.json();
 
-        setUserMetadata(user_metadata);
-      } catch (e) {
-        let message = 'Unknown Error';
-        if (e instanceof Error) message = e.message;
-        console.log({ message });
-      }
-    };
+  //       setUserMetadata(user_metadata);
+  //     } catch (e) {
+  //       let message = 'Unknown Error';
+  //       if (e instanceof Error) message = e.message;
+  //       console.warn({ message });
+  //     }
+  //   };
 
-    getUserMetadata();
-  }, [getAccessTokenSilently, user?.sub]);
+  //   getUserMetadata();
+  // }, [getAccessTokenSilently, user?.sub, isAuthenticated]);
   if (isLoading) {
     return <div>Loading ...</div>;
   }
-  if (isAuthenticated) {
-    return (
-      <div>
-        <img src={user?.picture} alt={user?.name} />
-        <hr />
-        <h2>{user?.name}</h2>
-        <p>{user?.email}</p>
-        {userMetadata ? (
-          <pre>{JSON.stringify(userMetadata, null, 2)}</pre>
-        ) : (
-          'No user metadata defined'
-        )}
-        <hr />
-        <button onClick={handleLogoutClick}>Logout</button>
-      </div>
-    );
-  }
-  return <div>User not authenticated</div>;
+  return (
+    <div>
+      <img src={user?.picture} alt={user?.name} />
+      <hr />
+      <h2>{user?.name}</h2>
+      <p>{user?.email}</p>
+      {userMetadata ? (
+        <pre>{JSON.stringify(userMetadata, null, 2)}</pre>
+      ) : (
+        'No user metadata defined'
+      )}
+      <hr />
+      <button onClick={handleLogoutClick}>Logout</button>
+    </div>
+  );
 };
 
 export default Profile;
