33e7124356061eaea6ef3ed552b4b42af5768308
Bus42 <gbrewton1980@gmail.com>
2022-06-30 17:37:23 -0600

    Adds fusionauth login to menu

--- src/components/organisms/Header/AuthNavItems.tsx
+++ src/components/organisms/Header/AuthNavItems.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import useHeaderContext from './useHeaderContext';
 
+const clientId = process.env.REACT_APP_FUSIONAUTH_CLIENT_ID;
+const baseUrl = process.env.REACT_APP_FUSIONAUTH_BASE_URL;
+
 export default function AuthNavItems({
   className,
 }: {
@@ -33,6 +36,13 @@ export default function AuthNavItems({
         <Link to="/login" onClick={closeMenu}>
           Log In
         </Link>
+      </li>
+      <li className={classnames('auth-nav', className)}>
+        <a
+          href={`${baseUrl}/oauth2/authorize?client_id=${clientId}&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000`}
+        >
+          Fusion Log In
+        </a>
       </li>
       <li className={classnames('auth-nav', className)}>
         <Link to="/signup" onClick={closeMenu}>
