3d5c54cff00f4436e5610adbfcd72cfa5be66136
Bus42 <gbrewton1980@gmail.com>
2022-07-07 13:35:40 -0600

    Replaces FusionAuth Login in menu

--- src/components/organisms/Header/AuthNavItems.tsx
+++ src/components/organisms/Header/AuthNavItems.tsx
@@ -1,7 +1,6 @@
 import { classnames } from '@story-squad/react-utils';
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { axiosWithoutAuth } from '../../../api/axiosWithConfig';
 import useHeaderContext from './useHeaderContext';
 
 export default function AuthNavItems({
@@ -11,14 +10,7 @@ export default function AuthNavItems({
 } = {}): React.ReactElement {
   const { user, closeMenu } = useHeaderContext();
 
-  const handleFusionLogin = async () => {
-    axiosWithoutAuth()
-      .get('/api/auth/login')
-      .then((res) => console.log(res))
-      .catch((err) => {
-        console.error(err);
-      });
-  };
+  const clientId = process.env.REACT_APP_FUSIONAUTH_CLIENT_ID;
   return user ? (
     <>
       <li className={classnames('auth-nav', className)}>
@@ -36,6 +28,13 @@ export default function AuthNavItems({
           My Stories
         </Link>
       </li>
+      <li className={classnames('auth-nav', className)}>
+        <a
+          href={`https://storysquad.fusionauth.io/oauth2/logout?client_id=${clientId}`}
+        >
+          Fusionauth Logout
+        </a>
+      </li>
     </>
   ) : (
     <>
@@ -50,7 +49,11 @@ export default function AuthNavItems({
         </Link>
       </li>
       <li className={classnames('auth-nav', className)}>
-        <button onClick={handleFusionLogin}>Fusion Login</button>
+        <a
+          href={`https://storysquad.fusionauth.io/oauth2/authorize?client_id=${clientId}&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000`}
+        >
+          FusionAuth Login
+        </a>
       </li>
     </>
   );
