ab7b11ddb46fa4ec66aecd295b9791567fc2ad5f
Bus42 <gbrewton1980@gmail.com>
2022-07-20 16:54:37 -0600

    Stores returned user data, token in local storage

--- src/components/views/DashboardView/DashboardView.tsx
+++ src/components/views/DashboardView/DashboardView.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import { FullscreenImageOverlay } from '../../modals';
 import {
   Leaderboard,
@@ -16,6 +16,24 @@ export interface DashboardViewProps {
 export default function DashboardView({
   submitVotes,
 }: DashboardViewProps): React.ReactElement {
+  useLayoutEffect((): void => {
+    // get cookie named 'user' from browser session
+    const user = document.cookie
+      .split('; ')
+      .find((cookie) => cookie.startsWith('user='));
+    if (user) {
+      // store in local storage as "logged:in:user"
+      localStorage.setItem('logged:in:user', JSON.stringify(user));
+    }
+    const token = document.cookie
+      .split('; ')
+      .find((cookie) => cookie.startsWith('token='));
+    if (token) {
+      // store in local storage as "token"
+      localStorage.setItem('token', JSON.stringify(token));
+    }
+    return;
+  }, []);
   return (
     <DashboardTemplate>
       <FullscreenImageOverlay />
