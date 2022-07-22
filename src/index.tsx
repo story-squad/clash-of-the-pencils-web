d1e964541fecb1ca5d1e13d9f4228cf95d16d1c8
Bus42 <gbrewton1980@gmail.com>
2022-07-22 17:01:33 -0600

    Removes unused attrs in AuthOProvider

--- src/index.tsx
+++ src/index.tsx
@@ -17,9 +17,7 @@ ReactDOM.render(
       <Auth0Provider
         domain={process.env.REACT_APP_AUTH0_DOMAIN || ''}
         clientId={process.env.REACT_APP_AUTH0_CLIENT_ID || ''}
-        redirectUri="http://localhost:3000/auth/auth0profile" // must match the redirectUri in your Auth0 app dashboard
-        audience={process.env.REACT_APP_AUTH0_AUDIENCE}
-        scope={process.env.REACT_APP_AUTH0_SCOPE}
+        redirectUri="http://localhost:3000/auth/auth0profile"
         useRefreshTokens={true}
       >
         <RecoilRoot>
