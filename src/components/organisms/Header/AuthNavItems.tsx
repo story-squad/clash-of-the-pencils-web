88b90cbf283daf7e26fbcf2a88582d8856910043
Bus42 <gbrewton1980@gmail.com>
2022-07-23 11:26:21 -0600

    Replaces login with Auth0 login

--- src/components/organisms/Header/AuthNavItems.tsx
+++ src/components/organisms/Header/AuthNavItems.tsx
@@ -2,6 +2,7 @@ import { classnames } from '@story-squad/react-utils';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import useHeaderContext from './useHeaderContext';
+import { useAuth0 } from '@auth0/auth0-react';
 
 export default function AuthNavItems({
   className,
@@ -9,6 +10,14 @@ export default function AuthNavItems({
   className?: string;
 } = {}): React.ReactElement {
   const { user, closeMenu } = useHeaderContext();
+  const { loginWithRedirect, isAuthenticated, logout } = useAuth0();
+  const handleLogoutClick = (): void => {
+    logout({
+      returnTo:
+        process.env.REACT_APP_AUTH0_CLIENT_ORIGIN_URL ||
+        'http://localhost:3000/',
+    });
+  };
   return user ? (
     <>
       <li className={classnames('auth-nav', className)}>
@@ -30,9 +39,21 @@ export default function AuthNavItems({
   ) : (
     <>
       <li className={classnames('auth-nav', className)}>
-        <Link to="/login" onClick={closeMenu}>
-          Log In
-        </Link>
+        {isAuthenticated ? (
+          <p
+            className={classnames('auth-nav', className)}
+            onClick={handleLogoutClick}
+          >
+            Logout
+          </p>
+        ) : (
+          <p
+            className={classnames('auth-nav', className)}
+            onClick={loginWithRedirect}
+          >
+            Log In
+          </p>
+        )}
       </li>
       <li className={classnames('auth-nav', className)}>
         <Link to="/signup" onClick={closeMenu}>
