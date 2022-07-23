f99cb90dbaf47d4377321fc566394b6593fc38ea
Bus42 <gbrewton1980@gmail.com>
2022-07-23 11:25:51 -0600

    Changes Auth0 redirect URI

--- src/index.tsx
+++ src/index.tsx
@@ -17,7 +17,7 @@ ReactDOM.render(
       <Auth0Provider
         domain={process.env.REACT_APP_AUTH0_DOMAIN || ''}
         clientId={process.env.REACT_APP_AUTH0_CLIENT_ID || ''}
-        redirectUri="http://localhost:3000/auth/auth0profile"
+        redirectUri="http://localhost:3000/"
         useRefreshTokens={true}
       >
         <RecoilRoot>
