c82e8b8c4a5c0b16f76369551342df092e1ee3ac
Bus42 <gbrewton1980@gmail.com>
2022-07-29 11:09:25 -0600

    comments WIP code

--- src/components/views/DashboardView/DashboardView.tsx
+++ src/components/views/DashboardView/DashboardView.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { FullscreenImageOverlay } from '../../modals';
 import {
   Leaderboard,
@@ -8,15 +8,15 @@ import {
   VotingOrganism,
 } from '../../organisms';
 import { DashboardTemplate } from '../../templates';
-import { useAuth0 } from '@auth0/auth0-react';
-import axios, { Method } from 'axios';
+// import { useAuth0 } from '@auth0/auth0-react';
+// import axios, { Method } from 'axios';
 
-interface Options {
-  method: Method | undefined;
-  url: string;
-  headers: Record<string, string>;
-  data: URLSearchParams;
-}
+// interface Options {
+//   method: Method | undefined;
+//   url: string;
+//   headers: Record<string, string>;
+//   data: URLSearchParams;
+// }
 
 export interface DashboardViewProps {
   submitVotes?: () => Promise<unknown>;
@@ -25,32 +25,30 @@ export interface DashboardViewProps {
 export default function DashboardView({
   submitVotes,
 }: DashboardViewProps): React.ReactElement {
-  const { user, isLoading, isAuthenticated } = useAuth0();
-  useEffect(() => {
-    const options: Options = {
-      method: 'POST',
-      url: 'https://dev-dwwemlm7.us.auth0.com/oauth/token',
-      headers: { 'content-type': 'application/x-www-form-urlencoded' },
-      data: new URLSearchParams({
-        grant_type: 'authorization_code',
-        client_id: process.env.REACT_APP_AUTH0_CLIENT_ID || '',
-        client_secret: process.env.REACT_APP_AUTH0_CLIENT_SECRET || '',
-        code: process.env.REACT_APP_AUTH0_CODE || 'what code?',
-        redirect_uri:
-          process.env.REACT_APP_AUTH0_REDIRECT_URI || 'http://localhost:3000/',
-      }),
-    };
-    // get tokens from Auth0
-    if (isAuthenticated) {
-      axios
-        .request(options)
-        .then((res) => console.log(res))
-        .catch((err) => console.error(err));
-    }
-  }, [isAuthenticated, user]);
-  return isLoading ? (
-    <div>Loading..</div>
-  ) : (
+  // const { user, isLoading, isAuthenticated } = useAuth0();
+  // useEffect(() => {
+  //   const options: Options = {
+  //     method: 'POST',
+  //     url: 'https://dev-dwwemlm7.us.auth0.com/oauth/token',
+  //     headers: { 'content-type': 'application/x-www-form-urlencoded' },
+  //     data: new URLSearchParams({
+  //       grant_type: 'authorization_code',
+  //       client_id: process.env.REACT_APP_AUTH0_CLIENT_ID || '',
+  //       client_secret: process.env.REACT_APP_AUTH0_CLIENT_SECRET || '',
+  //       code: process.env.REACT_APP_AUTH0_CODE || 'what code?',
+  //       redirect_uri:
+  //         process.env.REACT_APP_AUTH0_REDIRECT_URI || 'http://localhost:3000/',
+  //     }),
+  //   };
+  //   // get tokens from Auth0
+  //   if (isAuthenticated) {
+  //     axios
+  //       .request(options)
+  //       .then((res) => console.log(res))
+  //       .catch((err) => console.error(err));
+  //   }
+  // }, [isAuthenticated, user]);
+  return (
     <DashboardTemplate>
       <FullscreenImageOverlay />
       <TwoColumn left={<Prompt />} right={<Leaderboard />} />
