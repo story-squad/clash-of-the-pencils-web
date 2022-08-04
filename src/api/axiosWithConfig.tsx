ad7b56270f97ee9d85f3175ac8df0f14b05070bc
Bus42 <gbrewton1980@gmail.com>
2022-08-04 16:37:35 -0600

    refactors axiosWithAuth to use Auth0 token

--- src/api/axiosWithConfig.tsx
+++ src/api/axiosWithConfig.tsx
@@ -1,5 +1,5 @@
 import axios, { AxiosInstance } from 'axios';
-import { token } from '../utils';
+import { useAuth0 } from '@auth0/auth0-react';
 
 // Attempts to read the API URL from your ENV, falls back to localhost
 const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
@@ -8,10 +8,14 @@ interface AxiosGeneratorProps {
   timeoutInSeconds?: number;
 }
 
+async function getAccessToken(): Promise<string> {
+  return await useAuth0().getAccessTokenSilently();
+}
+
 export const axiosWithAuth = ({
   timeoutInSeconds = 0,
 }: AxiosGeneratorProps = {}): AxiosInstance => {
-  const Authorization = token.get();
+  const Authorization = getAccessToken();
   return axios.create({
     baseURL,
     headers: { Authorization },
