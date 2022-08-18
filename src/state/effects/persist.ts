4630c4eea409ad9aefbfd17e4bf56145b1b82ef5
Bus42 <gbrewton1980@gmail.com>
2022-08-18 11:10:12 -0600

    Adds try/catch blocks to persist.ts

--- src/state/effects/persist.ts
+++ src/state/effects/persist.ts
@@ -10,29 +10,37 @@ export const persist: <DataType>(
   ({ onSet, setSelf }) => {
     const asString = config?.asString ?? false;
 
-    // Initialize on app load
-    const initVal = localStorage.getItem(key);
-    if (initVal !== null) {
-      if (asString) {
-        // HACK - added type interface hack here so that we can store this as JUST a string and keep linter happy :)
-        setSelf(initVal as unknown as DefaultValue);
-      } else {
-        const parsedInitVal = JSON.parse(initVal);
-        setSelf(parsedInitVal);
-      }
-    }
-
-    // And add the onSet atom event handler
-    onSet((newVal) => {
-      if (newVal instanceof DefaultValue) {
-        localStorage.removeItem(key);
-      } else {
+    try {
+      // Initialize on app load
+      const initVal = localStorage.getItem(key);
+      if (initVal !== null) {
         if (asString) {
           // HACK - added type interface hack here so that we can store this as JUST a string and keep linter happy :)
-          localStorage.setItem(key, newVal as unknown as string);
+          setSelf(initVal as unknown as DefaultValue);
         } else {
-          localStorage.setItem(key, JSON.stringify(newVal));
+          const parsedInitVal = JSON.parse(initVal);
+          setSelf(parsedInitVal);
         }
       }
-    });
+    } catch (e) {
+      console.error(e);
+    }
+
+    try {
+      // And add the onSet atom event handler
+      onSet((newVal) => {
+        if (newVal instanceof DefaultValue) {
+          localStorage.removeItem(key);
+        } else {
+          if (asString) {
+            // HACK - added type interface hack here so that we can store this as JUST a string and keep linter happy :)
+            localStorage.setItem(key, newVal as unknown as string);
+          } else {
+            localStorage.setItem(key, JSON.stringify(newVal));
+          }
+        }
+      });
+    } catch (e) {
+      console.error(e);
+    }
   };
