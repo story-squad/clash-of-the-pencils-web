13ce3c48e63b29c303e3a066ed2c1960e8e0246a
Greg Brewton <gbrewton1980@gmail.com>
2022-09-06 18:30:00 -0600

    Adds jsdoc, deprecation warning

--- src/api/Users/checkAvailability.ts
+++ src/api/Users/checkAvailability.ts
@@ -8,6 +8,15 @@ export async function isCodenameAvailable(codename: string): Promise<boolean> {
   return data.CodeNameorEmailavailable;
 }
 
+// Not going to need this since Auth0 handles it
+/**
+ * @function isEmailAvailable
+ * @deprecated Login is handled by Auth0
+ * @async
+ * @param email {string} - The email to check
+ * @returns {Promise<boolean>} - Whether or not the email is available
+ * @description Calls the API to check if the email is available
+ */
 export async function isEmailAvailable(email: string): Promise<boolean> {
   const { data } = await axiosWithoutAuth().get(
     `/api/users/availability?email=${email}`,
