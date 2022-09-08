fb9db33f171a2bf8c32fca037587cd1f4ecb12e1
Greg Brewton <gbrewton1980@gmail.com>
2022-09-07 18:45:06 -0600

    Attaches token to header, polishes inputs

--- src/components/forms/SignupForm/SignupForm.tsx
+++ src/components/forms/SignupForm/SignupForm.tsx
@@ -10,17 +10,20 @@ interface SignupFormValues {
   birthday: string;
   parentEmail: string;
   termsOfService: boolean;
-  codeName: string;
+  codename: string;
 }
 
 const SignupForm = (): React.ReactElement => {
-  const { user } = useAuth0();
+  const { user, getAccessTokenSilently } = useAuth0();
   const {
     register,
     handleSubmit,
     formState: { errors },
     watch,
   } = useForm<SignupFormValues>();
+  useEffect(() => {
+    if (Object.keys(errors).length > 0) console.warn(errors);
+  }, [errors]);
   const onSubmit = (data: SignupFormValues) => {
     console.groupCollapsed('Signup Form Submitted');
     console.log('%cUser data from Auth0 Provider', 'color: #00b4d8');
@@ -28,38 +31,48 @@ const SignupForm = (): React.ReactElement => {
     console.log('%cForm data', 'color: #00b4d8');
     console.table(data);
     console.groupEnd();
-    useEffect(() => {
-      if (Object.keys(errors).length > 0) console.warn(errors);
-    }, [errors]);
-    axios.patch(
-      `https://${process.env.REACT_APP_AUTH0_DOMAIN}/api/v2/users/${user?.sub}`,
-      {
-        user_metadata: {
-          codeName: data.codeName || user?.nickname,
-        },
-        app_metadata: {
-          lastName: data.lastName || user?.family_name,
-          firstName: data.firstName || user?.given_name,
-          parentEmail: data.parentEmail,
-          birthday: data.birthday,
-          termsOfService: data.termsOfService,
-        },
-      },
-    );
+    getAccessTokenSilently()
+      .then((token) => {
+        axios.patch(
+          `${process.env.REACT_APP_AUTH0_AUDIENCE}/users/${user?.sub}`,
+          {
+            user_metadata: {
+              codename: data.codename || user?.nickname,
+            },
+            app_metadata: {
+              lastName: data.lastName || user?.family_name,
+              firstName: data.firstName || user?.given_name,
+              parentEmail: data.parentEmail,
+              birthday: data.birthday,
+              termsOfService: data.termsOfService,
+            },
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          },
+        );
+      })
+      .catch((err) => {
+        console.warn(err);
+      });
   };
   // console.log(errors);
   const watchBirthday = watch('birthday');
   const watchTermsOfService = watch('termsOfService');
-  const watchCodeName = watch('codeName');
+  const watchCodeName = watch('codename');
   const currentYear = new Date().getFullYear();
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       {!user?.given_name && (
         <label htmlFor="firstName">
+          First Name
           <input
             id="firstName"
             type="text"
-            value={user?.given_name}
+            placeholder="First Name"
+            autoComplete="given-name"
             {...register('firstName', { required: true, maxLength: 80 })}
           />
         </label>
@@ -70,19 +83,21 @@ const SignupForm = (): React.ReactElement => {
           <input
             id="lastName"
             type="text"
-            placeholder="Last name"
+            placeholder="Last Name"
+            autoComplete="family-name"
             {...register('lastName', { required: true, maxLength: 100 })}
           />
         </label>
       )}
-      {/* codeName input */}
-      <label htmlFor="codeName">
+      {/* codename input */}
+      <label htmlFor="codename">
         Code Name
         <input
-          id="codeName"
+          id="codename"
           type="text"
-          placeholder="Code Name"
-          {...register('codeName', { required: true, maxLength: 100 })}
+          placeholder="Codename"
+          autoComplete="nickname"
+          {...register('codename', { required: true, maxLength: 100 })}
         />
       </label>
       {/* birthday input */}
@@ -91,6 +106,7 @@ const SignupForm = (): React.ReactElement => {
         <input
           id="birthday"
           type="date"
+          autoComplete="bday"
           {...register('birthday', { required: true })}
         />
       </label>
