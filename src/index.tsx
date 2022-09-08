b81a690738fdbb336e32d7cdd8ee0c5fd7a9b379
Greg Brewton <gbrewton1980@gmail.com>
2022-09-08 17:05:23 -0600

    Adds check for .env variables

--- src/index.tsx
+++ src/index.tsx
@@ -11,16 +11,25 @@ import App from './App';
 
 dotenv.config();
 
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+const audience = process.env.REACT_APP_AUTH0_AUDIENCE;
+if (!domain || !clientId || !audience) {
+  throw new Error(
+    'Missing Auth0 environment variables. Please check your .env file. So long, and thanks for all the fish! 🐬',
+  );
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <HelmetProvider>
       <Auth0Provider
-        domain={`https://${process.env.REACT_APP_AUTH0_DOMAIN}` || ''}
-        clientId={process.env.REACT_APP_AUTH0_CLIENT_ID || ''}
-        redirectUri={window.location.origin}
+        domain={domain || ''}
+        clientId={clientId || ''}
+        redirectUri={`${window.location.origin}`}
         useRefreshTokens={true}
         cacheLocation="localstorage"
-        audience={process.env.REACT_APP_AUTH0_AUDIENCE} // this is the identifier found in the Auth0 dashboard under Applications > APIs > Clash API
+        audience={audience} // this is the identifier found in the Auth0 dashboard under Applications > APIs > Clash API
         // these are the scopes listed in the Auth0 dashboard under Applications > APIs > Auth0 Management API > Permissions
         scope="
         read:users
