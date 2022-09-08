9170a93c0b290a051bbcf4fa63c664e3455f1f63
Greg Brewton <gbrewton1980@gmail.com>
2022-09-08 13:13:35 -0600

    Edits scope declaration

--- src/index.tsx
+++ src/index.tsx
@@ -23,14 +23,14 @@ ReactDOM.render(
         audience={process.env.REACT_APP_AUTH0_AUDIENCE} // this is the identifier found in the Auth0 dashboard under Applications > APIs > Clash API
         // these are the scopes listed in the Auth0 dashboard under Applications > APIs > Auth0 Management API > Permissions
         scope="
-        read:users,
-        update:users,
-        delete:users,
-        create:users,
-        read: users_app_metadata,
-        update: users_app_metadata,
-        delete: users_app_metadata,
-        create: users_app_metadata,
+        read:users
+        update:users
+        delete:users
+        create:users
+        read:users_app_metadata
+        update:users_app_metadata
+        delete:users_app_metadata
+        create:users_app_metadata
         "
       >
         <RecoilRoot>
