bf81942bade1d50a05f870f94057895e593a747e
Greg Brewton <gbrewton1980@gmail.com>
2022-09-09 16:11:57 -0600

    Removes login, adds cancel button to signup

--- src/components/views/SignupView/SignupView.tsx
+++ src/components/views/SignupView/SignupView.tsx
@@ -22,38 +22,26 @@ export default function SignupView(): React.ReactElement {
     reValidateMode: 'onChange',
     shouldFocusError: true,
   });
-  // const { push } = useHistory();
-  // refactor to use loginWithRedirect from Auth0
-  // const login = useSetRecoilState(auth.login);
-  // needs to post user object to auth0 user database on submission
-  // const submitHandler = useCallback(
-  //   onSubmit ??
-  //     (async (data: Users.INewUser) => {
-  //       Reflect.deleteProperty(data, 'confirmPassword');
-  //       const res = await (() => {
-  //         if (isNew && cleverId && roleId)
-  //           return Clever.signupWithClever(data, roleId, cleverId);
-  //         return Auth.signup(data);
-  //       })();
-  //       login(res);
-  //       push('/');
-  //     }),
-  //   [login, onSubmit],
-  // );
-  const { loginWithRedirect } = useAuth0();
+  const { logout } = useAuth0();
+  /**
+   * @title handleCancel
+   * @description Cancels the signup process by clearing web storage, logging the user out of Auth0, and redirecting them to the home page.
+   */
+  const handleCancel = () => {
+    sessionStorage.clear();
+    localStorage.clear();
+    logout();
+  };
 
   return (
     <DashboardTemplate useStorySquadHeader className="signup-view">
       <div className="signup-header">
-        <h2>Sign Up Using Email Address</h2>
+        <h2>Complete Your Registration</h2>
       </div>
       <FormProvider {...methods}>
         <SignupForm />
       </FormProvider>
-      <p className="form-footer">
-        Already have an account?{' '}
-        <span onClick={() => loginWithRedirect()}>Sign In Here</span>
-      </p>
+      <button onClick={handleCancel}>Cancel Registration</button>
     </DashboardTemplate>
   );
 }
