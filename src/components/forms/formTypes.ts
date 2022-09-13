1b60c3961b8ea59016579ca73848e93fb6b87732
Greg Brewton <gbrewton1980@gmail.com>
2022-09-13 16:58:57 -0600

    Adds type ErrorWithBody

--- src/components/forms/formTypes.ts
+++ src/components/forms/formTypes.ts
@@ -8,8 +8,15 @@ export type FormOnSuccess<Response> = (
   response?: Response,
 ) => void | Promise<void>;
 
+declare type ErrorWithBody = Error & {
+  body?: Record<string, unknown>;
+};
+declare type SubmitType = Record<string, unknown> | FormData;
+declare type ErrorType = void | ErrorWithBody | Promise<void | ErrorWithBody>;
+declare type SuccessType = void | Promise<void>;
+
 export type FormProps = {
-  onSubmit: (data: Record<string, unknown>) => Promise<void>;
-  onError: (error: unknown) => void | unknown;
-  onSuccess: () => void;
+  onSubmit: (data?: SubmitType) => Promise<void>;
+  onError?: (err: unknown) => ErrorType;
+  onSuccess: () => SuccessType;
 };
