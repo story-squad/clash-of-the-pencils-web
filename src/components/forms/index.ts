cf5e53af2c09d10ca3dda09d1892c16e3d888158
Greg Brewton <gbrewton1980@gmail.com>
2022-09-13 16:59:23 -0600

    Removes LoginForm

--- src/components/forms/index.ts
+++ src/components/forms/index.ts
@@ -2,7 +2,6 @@ export * from './EditAccountForm';
 export * from './EmailForm';
 export * from './EmptyForm';
 export * as FormTypes from './formTypes';
-export * from './LoginForm';
 export * from './ResetPasswordForm';
 export * from './SignupForm';
 export * from './SubmissionForm';
