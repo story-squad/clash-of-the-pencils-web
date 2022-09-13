805696640a5a3c03de2719e985241468b4d3cd2f
Greg Brewton <gbrewton1980@gmail.com>
2022-09-13 16:05:39 -0600

    Changes type declarations

--- src/components/forms/EmailForm/EmailForm.tsx
+++ src/components/forms/EmailForm/EmailForm.tsx
@@ -2,22 +2,22 @@ import { useAsync } from '@story-squad/react-utils';
 import React, { useCallback } from 'react';
 import { useFormContext } from 'react-hook-form';
 import { Button, LoadIcon } from '../../atoms';
-import { FormProps } from '../formTypes';
 import { authFormInputs } from '../inputs';
 import './styles/index.scss';
 
-export type EmailFormProps = FormProps<{ email: string }>;
+export type EmailFormProps = {
+  onSubmit: (data: Record<string, unknown>) => Promise<void>;
+  onError: (error: unknown) => void;
+  onSuccess: () => void;
+};
 
-export default function EmailForm({
-  onSubmit,
-  onError,
-  onSuccess,
-}: EmailFormProps): React.ReactElement {
+const EmailForm = (props: EmailFormProps): React.ReactElement => {
+  const { onSubmit, onError, onSuccess } = props;
   const { handleSubmit, clearErrors } = useFormContext();
   const clearFormError = useCallback(() => clearErrors('form'), [clearErrors]);
 
   const [submitForm, isSubmitting] = useAsync({
-    run: async (data: { email: string }) => {
+    run: async (data: Record<string, string>) => {
       await onSubmit(data);
     },
     onError,
@@ -36,4 +36,6 @@ export default function EmailForm({
       </Button>
     </form>
   );
-}
+};
+
+export default EmailForm;
--- src/components/forms/formTypes.ts
+++ src/components/forms/formTypes.ts
@@ -1,16 +1,15 @@
 export type FormOnSubmit<FormData = unknown, Response = unknown> = (
   data: FormData,
 ) => Response | Promise<Response>;
+
 export type FormOnError<Error> = (err: Error) => void | Promise<void>;
+
 export type FormOnSuccess<Response> = (
   response?: Response,
 ) => void | Promise<void>;
-export interface FormProps<
-  FormData extends unknown = unknown,
-  Response = unknown,
-  Error = unknown,
-> {
-  onSubmit: FormOnSubmit<FormData, Response>;
-  onSuccess?: FormOnSuccess<Response>;
-  onError?: FormOnError<Error>;
-}
+
+export type FormProps = {
+  onSubmit: (data: Record<string, unknown>) => Promise<void>;
+  onError: (error: unknown) => void | unknown;
+  onSuccess: () => void;
+};
