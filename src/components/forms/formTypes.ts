a29a7ef07a74a41827ad3b1178844c02b45faef5
Greg Brewton <gbrewton1980@gmail.com>
2022-09-15 16:33:19 -0600

    Reverts prior changes to formProps

--- src/components/forms/formTypes.ts
+++ src/components/forms/formTypes.ts
@@ -7,16 +7,12 @@ export type FormOnError<Error> = (err: Error) => void | Promise<void>;
 export type FormOnSuccess<Response> = (
   response?: Response,
 ) => void | Promise<void>;
-
-declare type ErrorWithBody = Error & {
-  body?: Record<string, unknown>;
-};
-declare type SubmitType = Record<string, unknown> | FormData;
-declare type ErrorType = void | ErrorWithBody | Promise<void | ErrorWithBody>;
-declare type SuccessType = void | Promise<void>;
-
-export type FormProps = {
-  onSubmit: (data?: SubmitType) => Promise<void>;
-  onError?: (err: unknown) => ErrorType;
-  onSuccess: () => SuccessType;
-};
+export interface FormProps<
+  FormData extends unknown = unknown,
+  Response = unknown,
+  Error = unknown,
+> {
+  onSubmit: FormOnSubmit<FormData, Response>;
+  onSuccess?: FormOnSuccess<Response>;
+  onError?: FormOnError<Error>;
+}
